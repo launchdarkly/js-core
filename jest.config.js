d0ec99b17e40b88777e2ccfbb5ee20a5cb922413
Ryan Lamb <4955475+kinyoklion@users.noreply.github.com>
2022-05-18 13:34:15 -0700

    Remove deleted line.

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   transform: {'^.+\\.ts?$': 'ts-jest'},
   testEnvironment: 'node',
-  // testRegex: '/__tests__/.*\\.(test|spec)?\\.(ts|tsx)$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
 };
