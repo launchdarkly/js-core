a7eb0955c289aa9df45f20367052df605df413ae
Ryan Lamb <4955475+kinyoklion@users.noreply.github.com>
2022-05-18 13:33:26 -0700

    Add comment about namespacing.

--- server-sdk-common/src/api/index.ts
+++ server-sdk-common/src/api/index.ts
@@ -8,6 +8,8 @@ export * from './subsystems/LDStreamProcessor';
 export * from './LDContext';
 export * from './LDUser';
 
+// These are items that should be less frequently used, and therefore they
+// are namespaced to reduce clutter amongst the top level exports.
 export * as integrations from './integrations';
 export * as interfaces from './interfaces';
 export * as subsystems from './subsystems';
