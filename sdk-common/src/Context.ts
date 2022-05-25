af34183d8a2bdf8a19eaf3a358be894ae99026b6
Ryan Lamb <4955475+kinyoklion@users.noreply.github.com>
2022-05-25 10:54:07 -0700

    Empty context.

---
+++ sdk-common/src/Context.ts
@@ -0,0 +1,19 @@
+type MultiKind = {
+  kind: 'multi',
+  isMulti: true,
+};
+
+type SingleKind = {
+  kind: string
+  isMulti: false
+};
+
+type Kind = MultiKind | SingleKind;
+
+export default class Context {
+  public readonly kind: Kind;
+
+  private contexts?: Context[];
+
+  private attributes?: Record<string, any>;
+}
