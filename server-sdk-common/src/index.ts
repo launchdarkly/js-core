8364d540785f89ebde5a7203adb074e24b2f2b70
Ryan Lamb <4955475+kinyoklion@users.noreply.github.com>
2022-07-14 13:39:06 -0700

    Copy over contract-tests from node, and run scripts, and arrange some imports.

--- server-sdk-common/src/index.ts
+++ server-sdk-common/src/index.ts
@@ -3,6 +3,8 @@ import BigSegmentStoreStatusProviderImpl from './BigSegmentStatusProviderImpl';
 
 export * as platform from './platform';
 export * from './api';
+export * from '@launchdarkly/js-sdk-common';
 
-export { LDClientImpl };
-export { BigSegmentStoreStatusProviderImpl };
+export {
+  LDClientImpl, BigSegmentStoreStatusProviderImpl,
+};
