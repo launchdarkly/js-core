cd4c3c82068887bd6c31315ee09dc15e57d0c0aa
Ryan Lamb <4955475+kinyoklion@users.noreply.github.com>
2022-07-20 15:47:37 -0700

    Expose big segments options.

--- server-sdk-common/src/options/Configuration.ts
+++ server-sdk-common/src/options/Configuration.ts
@@ -2,6 +2,7 @@ import {
   LDLogger, NumberWithMinimum, TypeValidator, TypeValidators,
 } from '@launchdarkly/js-sdk-common';
 import {
+  LDBigSegmentsOptions,
   LDOptions, LDProxyOptions, LDStreamProcessor, LDTLSOptions,
 } from '../api';
 import { LDFeatureStore } from '../api/subsystems';
@@ -193,6 +194,8 @@ export default class Configuration {
 
   public readonly updateProcessor?: LDStreamProcessor;
 
+  public readonly bigSegments?: LDBigSegmentsOptions;
+
   constructor(options: LDOptions = {}) {
     // The default will handle undefined, but not null.
     // Because we can be called from JS we need to be extra defensive.
@@ -223,7 +226,7 @@ export default class Configuration {
       this.featureStore = validatedOptions.featureStore;
     }
 
-    // TODO: bigSegments
+    this.bigSegments = validatedOptions.bigSegments;
     this.updateProcessor = validatedOptions.updateProcessor;
     this.flushInterval = validatedOptions.flushInterval;
     this.pollInterval = validatedOptions.pollInterval;
