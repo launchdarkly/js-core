809db0ed1ef9b6d85a0bc6af2a39bfb628435977
Ryan Lamb <4955475+kinyoklion@users.noreply.github.com>
2022-12-21 12:09:26 -0800

    Add gulp.

---
+++ platform-fastly/gulpfile.js
@@ -0,0 +1,24 @@
+var gulp = require("gulp");
+var browserify = require("browserify");
+var source = require("vinyl-source-stream");
+var tsify = require("tsify");
+// var paths = {
+//   pages: ["src/*.html"],
+// };
+// gulp.task("copy-html", function () {
+//   return gulp.src(paths.pages).pipe(gulp.dest("dist"));
+// });
+gulp.task("default", function () {
+    return browserify({
+      basedir: ".",
+      debug: true,
+      entries: ["src/index.ts"],
+      cache: {},
+      packageCache: {},
+    })
+      .plugin(tsify)
+      .bundle()
+      .pipe(source("bundle.js"))
+      .pipe(gulp.dest("gulped"));
+  }
+);
