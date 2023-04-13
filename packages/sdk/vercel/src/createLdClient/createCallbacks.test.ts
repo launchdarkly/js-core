1b176c0548bf632a7c069dc285452d34c33059f3
Fabian Feldberg <ffeldberg@launchdarkly.com>
2023-04-13 14:59:30 +0100

    add tests

---
+++ packages/sdk/vercel/src/createLdClient/createCallbacks.test.ts
@@ -0,0 +1,59 @@
+import { EventEmitter } from 'node:events';
+import createCallbacks from './createCallbacks';
+import noop from '../utils/noop';
+
+describe('createCallbacks', () => {
+  let emitter: EventEmitter;
+  const err = new Error('test error');
+
+  beforeEach(() => {
+    emitter = new EventEmitter();
+    emitter.emit = jest.fn();
+  });
+
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
+  test('onError', () => {
+    emitter.on('error', noop);
+
+    const { onError } = createCallbacks(emitter);
+    onError(err);
+
+    expect(emitter.emit).toHaveBeenNthCalledWith(1, 'error', err);
+  });
+
+  test('onError should not be called', () => {
+    const { onError } = createCallbacks(emitter);
+    onError(err);
+
+    expect(emitter.emit).not.toHaveBeenCalled();
+  });
+
+  test('onFailed', () => {
+    const { onFailed } = createCallbacks(emitter);
+    onFailed(err);
+
+    expect(emitter.emit).toHaveBeenNthCalledWith(1, 'failed', err);
+  });
+
+  test('onReady', () => {
+    const { onReady } = createCallbacks(emitter);
+    onReady();
+
+    expect(emitter.emit).toHaveBeenNthCalledWith(1, 'ready');
+  });
+
+  test('onUpdate should be noop', () => {
+    const { onUpdate } = createCallbacks(emitter);
+
+    expect(onUpdate.toString()).toEqual(noop.toString());
+  });
+
+  test('hasEventListeners', () => {
+    const { hasEventListeners } = createCallbacks(emitter);
+
+    expect(hasEventListeners()).toBeFalsy();
+  });
+});
