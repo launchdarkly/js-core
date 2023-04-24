bfd66c45f01e5cfdfb45e568ce5714fcf44c7e3c
Fabian Feldberg <ffeldberg@launchdarkly.com>
2023-04-24 10:25:37 +0100

    add tests for createPlatformInfo

---
+++ packages/sdk/vercel/src/createPlatformInfo.test.ts
@@ -0,0 +1,22 @@
+import { name, version } from '../package.json';
+
+import createPlatformInfo from './createPlatformInfo';
+
+describe('Vercel Platform Info', () => {
+  it('platformData shows correct information', () => {
+    const platformData = createPlatformInfo();
+
+    expect(platformData.platformData()).toEqual({
+      name: 'Vercel Edge',
+    });
+  });
+
+  it('sdkData shows correct information', () => {
+    const platformData = createPlatformInfo();
+
+    expect(platformData.sdkData()).toEqual({
+      name: name,
+      version: version,
+    });
+  });
+});
