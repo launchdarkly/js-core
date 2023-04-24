8bfd57c67efc02dba8241b7e579b021418a72f62
Fabian Feldberg <ffeldberg@launchdarkly.com>
2023-04-24 10:44:48 +0100

    working tests for index.ts, need to fix types

---
+++ packages/sdk/vercel/src/index.test.ts
@@ -0,0 +1,50 @@
+import type { EdgeConfigClient } from '@vercel/edge-config';
+import { LDClient } from '@launchdarkly/js-server-sdk-common-edge';
+import { init } from './index';
+import * as testData from './utils/testData.json';
+import mockEdge from './utils/mockEdge';
+
+
+const sdkKey = 'test-sdk-key';
+const flagKey = 'testFlag1';
+const context = { kind: 'user', key: 'test-user-key-1' };
+
+describe('init', () => {
+  let ldClient: LDClient;
+
+  beforeAll(async () => {
+    // @ts-ignore
+    mockEdge.get = jest.fn(((key: string) => testData))
+    ldClient = init(sdkKey, mockEdge);
+    await ldClient.waitForInitialization();
+  });
+
+  afterAll(() => {
+    ldClient.close();
+  });
+
+  test('variation', async () => {
+    const flagDetail = await ldClient.variation(flagKey, context, false);
+    expect(flagDetail).toBeTruthy();
+  });
+
+  test('variationDetail', async () => {
+    const flagDetail = await ldClient.variationDetail(flagKey, context, false);
+    expect(flagDetail).toEqual({ reason: { kind: 'FALLTHROUGH' }, value: true, variationIndex: 0 });
+  });
+
+  test('allFlags', async () => {
+    const allFlags = await ldClient.allFlagsState(context);
+
+    expect(allFlags).toBeDefined();
+    expect(allFlags.toJSON()).toEqual({
+      $flagsState: {
+        testFlag1: { debugEventsUntilDate: null, variation: 0, version: 2 },
+        testFlag2: { debugEventsUntilDate: null, variation: 1, version: 2 },
+      },
+      $valid: true,
+      testFlag1: true,
+      testFlag2: false,
+    });
+  });
+});
