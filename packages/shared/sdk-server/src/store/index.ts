b8f8fc6d3f215a061744b21eec33960404b11388
Yusinto Ngadiman <yusinto@gmail.com>
2023-04-27 17:31:17 -0700

    chore: remove eslint comment

--- packages/shared/sdk-server/src/store/index.ts
+++ packages/shared/sdk-server/src/store/index.ts
@@ -1,5 +1,4 @@
 import AsyncStoreFacade from './AsyncStoreFacade';
 import { deserializePoll } from './serialization';
 
-// eslint-disable-next-line import/prefer-default-export
 export { AsyncStoreFacade, deserializePoll };
