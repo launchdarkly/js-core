357a92e476f66bc4a317b176821820a99a66529b
Ryan Lamb <4955475+kinyoklion@users.noreply.github.com>
2023-06-07 11:23:16 -0700

    Ensure correct key types are exported for the persistent store.

--- packages/shared/sdk-server/src/api/interfaces/persistent_store/index.ts
+++ packages/shared/sdk-server/src/api/interfaces/persistent_store/index.ts
@@ -1,6 +1,15 @@
 import ItemDescriptor from './ItemDescriptor';
-import PersistentDataStore from './PersistentDataStore';
+import KeyedItem from './KeyedItem';
+import PersistentDataStore, { KindKeyedStore } from './PersistentDataStore';
+
 import PersistentStoreDataKind from './PersistentStoreDataKind';
 import SerializedItemDescriptor from './SerializedItemDescriptor';
 
-export { ItemDescriptor, PersistentDataStore, PersistentStoreDataKind, SerializedItemDescriptor };
+export {
+  ItemDescriptor,
+  PersistentDataStore,
+  PersistentStoreDataKind,
+  SerializedItemDescriptor,
+  KeyedItem,
+  KindKeyedStore,
+};
