cbdcb5f0726b4ed1b155a10b449265d794f0c779
Darius Harrison <dharrison@launchdarkly.com>
2023-06-08 10:35:37 -0500

    Adding tests for index files

--- packages/sdk/akamai-base/src/index.test.ts
+++
@@ -1,7 +0,0 @@
-/* Adding this file as a temporary file until I fix the actual test */
-
-describe('temporary test file', () => {
-  it('should pass', () => {
-    expect(true).toBeTruthy();
-  });
-});
