97d47c12da53bb46c33ae368889668a5d8c05e8a
Ryan Lamb <4955475+kinyoklion@users.noreply.github.com>
2023-06-08 09:14:13 -0700

    Work on implementation.

--- packages/store/node-server-sdk-dynamodb/src/DynamoDBClientState.ts
+++ packages/store/node-server-sdk-dynamodb/src/DynamoDBClientState.ts
@@ -1,3 +1,12 @@
+import { LDDynamoDBOptions } from './LDDynamoDBOptions';
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
+import { LDLogger } from '@launchdarkly/node-server-sdk';
+
+// Unlike some other database integrations where the key prefix is mandatory and has
+// a default value, in DynamoDB it is fine to not have a prefix. If there is one, we
+// prepend it to keys with a ':' separator.
+const DEFAULT_PREFIX = '';
+
 
 /**
  * Class for managing the state of a dynamodb client.
@@ -7,5 +16,32 @@
  * @internal
  */
 export default class DynamoDBClientState {
-  constructor()
+  // This will include the ':' if a prefix is set.
+  private prefix: string;
+
+  private client: DynamoDBClient;
+
+  constructor(options: LDDynamoDBOptions, logger?: LDLogger) {
+    this.prefix = options?.prefix ? `${options!.prefix}:` : DEFAULT_PREFIX;
+
+    if(options.dynamoDBClient) {
+      this.client = options.dynamoDBClient;
+    } else if(options.clientOptions) {
+      this.client = new DynamoDBClient(options!.clientOptions);
+    } else {
+      // TODO: Kaboom
+    }
+
+    // Unlike some other database integrations, we don't need to keep track of whether we
+    // created our own client so as to shut it down later; the AWS client is stateless.
+  }
+
+  /**
+   * Get a key with prefix prepended.
+   * @param key The key to prefix.
+   * @returns The prefixed key.
+   */
+  prefixedKey(key: string): string {
+    return `${this.prefix}${key}`;
+  }
 }
--- packages/store/node-server-sdk-dynamodb/src/LDDynamoDBOptions.ts
+++ packages/store/node-server-sdk-dynamodb/src/LDDynamoDBOptions.ts
@@ -1,5 +1,5 @@
 import { LDLogger } from '@launchdarkly/node-server-sdk';
-import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
+import { DynamoDBClient, DynamoDBClientConfig } from '@aws-sdk/client-dynamodb';
 
 /**
  * Options for configuring {@link DynamoDBFeatureStoreFactory} or {@link DynamoDBBigSegmentStoreFactory}.
@@ -8,14 +8,14 @@ export interface LDDynamoDBOptions {
   /**
    * Options to be passed to the DynamoDB client constructor, as defined by the AWS SDK.
    */
-  clientOptions?: DynamoDB.DocumentClient.DocumentClientOptions & DynamoDB.Types.ClientConfiguration;
+  clientOptions?: DynamoDBClientConfig;
 
   /**
    * Specifies an existing, already-configured DynamoDB client instance that the feature store
    * should use rather than creating one of its own. If you specify an existing client, then the
    * clientOptions property is ignored.
    */
-  dynamoDBClient?: DynamoDB.DocumentClient;
+  dynamoDBClient?: DynamoDBClient;
 
   /**
    * An optional namespace prefix for all keys stored in DynamoDB. Use this if you are sharing
@@ -28,8 +28,8 @@ export interface LDDynamoDBOptions {
    * The amount of time, in seconds, that recently read or updated items should remain in an
    * in-memory cache. If it is zero, there will be no in-memory caching.
    *
-   * This parameter applies only to [[DynamoDBFeatureStore]]. It is ignored for [[DynamoDBBigSegmentStore]].
-   * Caching for [[DynamoDBBigSegmentStore]] is configured separately, in the SDK's
+   * This parameter applies only to {@link DynamoDBFeatureStore}. It is ignored for {@link DynamoDBBigSegmentStore}.
+   * Caching for {@link DynamoDBBigSegmentStore} is configured separately, in the SDK's
    * `LDBigSegmentsOptions` type, since it is independent of what database implementation is used.
    *
    * If omitted, the default value is 15 seconds.
