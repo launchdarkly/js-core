aac91420e31fcbac50e99ca5efbac02d6437cc69
Ryan Lamb <4955475+kinyoklion@users.noreply.github.com>
2023-06-09 15:23:30 -0700

    Fix name of big segement store factory. Update index.

--- packages/store/node-server-sdk-redis/src/index.ts
+++ packages/store/node-server-sdk-redis/src/index.ts
@@ -1,2 +1,3 @@
 export { default as RedisFeatureStoreFactory } from './RedisFeatureStoreFactory';
+export { default as RedisBigSegmentStoreFactory } from './RedisBigSegmentStoreFactory';
 export { default as LDRedisOptions } from './LDRedisOptions';
