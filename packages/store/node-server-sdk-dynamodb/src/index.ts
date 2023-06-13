61af43ce630dbff080e18778b55f21a9619d4c21
Ryan Lamb <4955475+kinyoklion@users.noreply.github.com>
2023-06-13 08:32:55 -0700

    Rename default exports for greater backward compatibility.

--- packages/store/node-server-sdk-dynamodb/src/index.ts
+++ packages/store/node-server-sdk-dynamodb/src/index.ts
@@ -1,3 +1,7 @@
-export { default as DynamoDBFeatureStoreFactory } from './DynamoDBFeatureStoreFactory';
-export { default as DynamoDBBigSegmentStoreFactory } from './DynamoDBBigSegmentStoreFactory';
+// Exporting the factories without the 'Factory'. This keeps them in-line with
+// previous store versions. The differentiation between the factory and the store
+// is not critical for consuming the SDK.
+export { default as DynamoDBFeatureStore } from './DynamoDBFeatureStoreFactory';
+export { default as DynamoDBBigSegmentStore } from './DynamoDBBigSegmentStoreFactory';
+
 export { default as LDDynamoDBOptions } from './LDDynamoDBOptions';
