2aac6eda273a1a540c27dcb6ad701605c535e6d3
Ryan Lamb <4955475+kinyoklion@users.noreply.github.com>
2023-07-25 13:51:26 -0700

    Documentation.

--- packages/shared/sdk-server/src/api/LDMigration.ts
+++ packages/shared/sdk-server/src/api/LDMigration.ts
@@ -9,6 +9,9 @@ import { LDMigrationStage } from './data/LDMigrationStage';
  */
 export type LDMigrationOrigin = 'old' | 'new';
 
+/**
+ * Result of a migration operation.
+ */
 export type LDMigrationResult<TResult> =
   | {
       success: true;
@@ -21,13 +24,36 @@ export type LDMigrationResult<TResult> =
       error: any;
     };
 
+/**
+ * Interface for a migration.
+ *
+ * TKTK
+ */
 export interface LDMigration<TMigrationRead, TMigrationWrite> {
+  /**
+   * TKTK
+   *
+   * @param key The key of the flag controlling the migration.
+   * @param context The context requesting the flag. The client will generate an analytics event to
+   *   register this context with LaunchDarkly if the context does not already exist.
+   * @param defaultValue The default migration step. Used if the value is not available from
+   * LaunchDarkly.
+   */
   read(
     key: string,
     context: LDContext,
     defaultValue: LDMigrationStage
   ): Promise<LDMigrationResult<TMigrationRead>>;
 
+  /**
+   * TKTK
+   *
+   * @param key The key of the flag controlling the migration.
+   * @param context The context requesting the flag. The client will generate an analytics event to
+   *   register this context with LaunchDarkly if the context does not already exist.
+   * @param defaultValue The default migration step. Used if the value is not available from
+   * LaunchDarkly.
+   */
   write(
     key: string,
     context: LDContext,
