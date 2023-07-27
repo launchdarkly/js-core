ceb38eac1cc2394f57449d7e483b2212080c84da
Ryan Lamb <4955475+kinyoklion@users.noreply.github.com>
2023-07-27 08:40:34 -0700

    Add some comments to events.

--- packages/shared/sdk-server/src/api/data/LDMigrationOpEvent.ts
+++ packages/shared/sdk-server/src/api/data/LDMigrationOpEvent.ts
@@ -2,6 +2,10 @@ import { LDEvaluationReason } from '@launchdarkly/js-sdk-common';
 
 export type LDMigrationOp = 'read' | 'write';
 
+/**
+ * Component of an LDMigrationOpEvent which tracks information about the
+ * evaluation of the migration flag.
+ */
 interface LDMigrationEvaluation {
   key: string;
   value: any;
@@ -10,6 +14,9 @@ interface LDMigrationEvaluation {
   reason: LDEvaluationReason;
 }
 
+/**
+ * Types of measurements supported by an LDMigrationOpEvent.
+ */
 export type LDMigrationMeasurement =
   | {
       key: 'latency' | 'error';
@@ -24,6 +31,12 @@ export type LDMigrationMeasurement =
       samplingOdds: number;
     };
 
+/**
+ * Event used to track information about a migration operation.
+ *
+ * Generally this event should not be created directly and instead an
+ * {@link MigrationOpTracker} should be used to generate it.
+ */
 export interface LDMigrationOpEvent {
   kind: 'migration_op';
   operation: LDMigrationOp;
