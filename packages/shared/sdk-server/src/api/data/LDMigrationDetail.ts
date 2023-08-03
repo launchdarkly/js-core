800e63928a7964f60c12c7790ff37114376dd910
Ryan Lamb <4955475+kinyoklion@users.noreply.github.com>
2023-08-03 14:54:59 -0700

    feat: Add custom event support to tracker.

--- packages/shared/sdk-server/src/api/data/LDMigrationDetail.ts
+++ packages/shared/sdk-server/src/api/data/LDMigrationDetail.ts
@@ -1,7 +1,11 @@
 import { LDEvaluationReason } from '@launchdarkly/js-sdk-common';
 
 import { LDMigrationOrigin } from '../LDMigration';
-import { LDMigrationOp, LDMigrationOpEvent } from './LDMigrationOpEvent';
+import {
+  LDMigrationCustomMeasurement,
+  LDMigrationOp,
+  LDMigrationOpEvent,
+} from './LDMigrationOpEvent';
 import { LDMigrationStage } from './LDMigrationStage';
 
 /**
@@ -19,10 +23,47 @@ export enum LDConsistencyCheck {
  * TKTK
  */
 export interface LDMigrationTracker {
+  /**
+   * Sets the migration related operation associated with these tracking measurements.
+   *
+   * @param op The operation being tracked.
+   */
   op(op: LDMigrationOp): void;
+
+  /**
+   * Report that an error has occurred for the specified origin.
+   *
+   * @param origin The origin of the error.
+   */
   error(origin: LDMigrationOrigin): void;
+
+  /**
+   * Report the result of a consistency check.
+   *
+   * @param result The result of the check.
+   */
   consistency(result: LDConsistencyCheck): void;
+
+  /**
+   * Report the latency of an operation.
+   *
+   * @param origin The origin the latency is being reported for.
+   * @param value The latency, in milliseconds, of the operation.
+   */
   latency(origin: LDMigrationOrigin, value: number): void;
+
+  /**
+   * Report a custom measurement. Unlike other methods on the tracker multiple custom
+   * measurements can be reported.
+   *
+   * @param measurement The custom measurement to track.
+   */
+  custom(measurement: LDMigrationCustomMeasurement): void;
+
+  /**
+   * Create a migration op event. If the event could not be created, because of a missing
+   * operation, then an event will not be returned.
+   */
   createEvent(): LDMigrationOpEvent | undefined;
 }
 
