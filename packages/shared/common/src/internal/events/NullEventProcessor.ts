b29c384dc9cd9718f0ee3fc7d95932f91de8ebd4
Yusinto Ngadiman <yusinto@gmail.com>
2023-08-03 14:32:29 -0700

    chore: fix mysterious broken ts compilation of NullEventProcessor

--- packages/shared/common/src/internal/events/NullEventProcessor.ts
+++ packages/shared/common/src/internal/events/NullEventProcessor.ts
@@ -1,23 +1,11 @@
-// This is an empty implementation, so it doesn't use this, and it has empty methods, and it
-// has unused variables.
-
-/* eslint-disable class-methods-use-this */
-
-/* eslint-disable @typescript-eslint/no-empty-function */
-
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { LDEventProcessor } from '../../api/subsystem';
-import InputEvent from './InputEvent';
 
-/**
- * Empty event processor implementation for when events are not desired.
- *
- * @internal
- */
 export default class NullEventProcessor implements LDEventProcessor {
-  close(): void {}
+  close() {}
 
-  async flush(): Promise<void> {}
+  async flush(): Promise<void> {
+    // empty comment to keep ts and eslint happy
+  }
 
-  sendEvent(inputEvent: InputEvent): void {}
+  sendEvent() {}
 }
