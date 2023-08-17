26bb7e5bc43305eb314a83a8f466a93fc34f279d
LaunchDarklyReleaseBot <launchdarklyreleasebot@launchdarkly.com>
2023-08-17 08:55:24 -0700

    Update index.ts

--- packages/shared/common/src/utils/index.ts
+++ packages/shared/common/src/utils/index.ts
@@ -4,5 +4,4 @@ import noop from './noop';
 import sleep from './sleep';
 import { VoidFunction } from './VoidFunction';
 
-// eslint-disable-next-line import/prefer-default-export
-export { defaultHeaders, httpErrorMessage, noop, LDHeaders, sleep, VoidFunction };
+export { defaultHeaders, httpErrorMessage, noop, LDHeaders, secondsToMillis, sleep, VoidFunction };
