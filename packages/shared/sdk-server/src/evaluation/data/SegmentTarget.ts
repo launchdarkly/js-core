491bae273c8e3f0eb5461db9c498637e2e02d34e
Ryan Lamb <4955475+kinyoklion@users.noreply.github.com>
2023-08-21 08:47:23 -0700

    Add comments.

--- packages/shared/sdk-server/src/evaluation/data/SegmentTarget.ts
+++ packages/shared/sdk-server/src/evaluation/data/SegmentTarget.ts
@@ -1,5 +1,8 @@
 export interface SegmentTarget {
   contextKind: string;
   values: string[];
+  // When there are a large number of values we put them into a set.
+  // This set is generated during deserialization, and changed back to a list
+  // during serialization.
   generated_valuesSet?: Set<string>;
 }
