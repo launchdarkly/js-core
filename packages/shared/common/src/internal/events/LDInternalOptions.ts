a3d3132e55277e3728bef5bbd6ef761779bffaac
Yusinto Ngadiman <yusinto@gmail.com>
2023-08-28 14:11:23 -0700

    chor: add comments

--- packages/shared/common/src/internal/events/LDInternalOptions.ts
+++ packages/shared/common/src/internal/events/LDInternalOptions.ts
@@ -1,3 +1,13 @@
+/**
+ * This is for internal use only.
+ *
+ * Edge sdks use clientSideID to query feature stores. They also send analytics
+ * using this clientSideID. This is a hybrid behavior because they are based
+ * on js-server-common, but uses the clientSideID instead of the sdkKey for the
+ * above reasons. These internal options allow the edge sdks to use the
+ * EventSender to send analytics to the correct LD endpoints using
+ * the clientSideId.
+ */
 export type LDInternalOptions = {
   analyticsEventPath?: string;
   diagnosticEventPath?: string;
