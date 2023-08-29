d90c7d9f75d5c61c4099afc1a519866dd4f6d537
Ryan Lamb <4955475+kinyoklion@users.noreply.github.com>
2023-08-29 09:42:53 -0700

    Remove unused scripts.

--- scripts/build-doc.sh
+++
@@ -1,9 +0,0 @@
-# Run this script like:
-# ./scripts/build-doc.sh packages/sdk/node
-
-set -e
-
-
-script_path=$(readlink -f "$0")
-cd "$script_path"/..
-npx typedoc --options "$1/typedoc.json"
--- scripts/doc-name.sh
+++
@@ -1,8 +0,0 @@
-# Given a path get the name for the documentation.
-# ./scripts/doc-name.sh packages/sdk/server-node
-# Produces something like:
-# LaunchDarkly Server-Side SDK for Node.js (0.1.0)
-
-set -e
-
-node -p "let pj = require('./$1/package.json');\`\${pj.description} (\${pj.version})\`";
