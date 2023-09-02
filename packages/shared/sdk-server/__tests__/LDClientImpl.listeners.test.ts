b16dca3551a151076f950a5bd25fac11808d0c82
LaunchDarklyReleaseBot <launchdarklyreleasebot@launchdarkly.com>
2023-09-02 14:52:11 -0700

    chore: restore all listeners unit tests.

--- packages/shared/sdk-server/__tests__/LDClientImpl.listeners.test.ts
+++ packages/shared/sdk-server/__tests__/LDClientImpl.listeners.test.ts
@@ -1,7 +1,11 @@
+import { AsyncQueue } from 'launchdarkly-js-test-helpers';
+
 import { internal } from '@launchdarkly/js-sdk-common';
 
-import { LDClientImpl } from '../src';
+import { AttributeReference, LDClientImpl } from '../src';
+import { Op } from '../src/evaluation/data/Clause';
 import TestData from '../src/integrations/test_data/TestData';
+import { makeFlagWithSegmentMatch } from './evaluation/flags';
 import TestLogger from './Logger';
 import makeCallbacks from './makeCallbacks';
 
@@ -12,12 +16,10 @@ const {
 describe('given an LDClient with test data', () => {
   let client: LDClientImpl;
   let td: TestData;
-  // let queue: AsyncQueue<string>;
-  let updateHandler: jest.Mock;
+  let queue: AsyncQueue<string>;
 
   beforeEach(() => {
-    // queue = new AsyncQueue();
-    updateHandler = jest.fn().mockName('updateHandler');
+    queue = new AsyncQueue();
     td = new TestData();
     client = new LDClientImpl(
       'sdk-key',
@@ -27,7 +29,7 @@ describe('given an LDClient with test data', () => {
         sendEvents: false,
         logger: new TestLogger(),
       },
-      { ...makeCallbacks(true), onUpdate: updateHandler },
+      { ...makeCallbacks(true), onUpdate: (key: string) => queue.add(key) },
     );
   });
 
@@ -36,116 +38,115 @@ describe('given an LDClient with test data', () => {
   });
 
   it('sends an event when a flag is added', async () => {
-    await td.update(td.flag('new-flag'));
-    // expect(await queue.take()).toEqual('new-flag');
-    expect(updateHandler).toHaveBeenCalledWith('new-flag');
+    td.update(td.flag('new-flag'));
+    expect(await queue.take()).toEqual('new-flag');
   });
 
-  // it('sends an event when a flag is updated', async () => {
-  //   td.update(td.flag('flag1').on(true));
-  //   td.update(td.flag('flag2').on(true));
-  //
-  //   expect(await queue.take()).toEqual('flag1');
-  //   expect(await queue.take()).toEqual('flag2');
-  //
-  //   td.update(td.flag('flag1').on(false));
-  //   td.update(td.flag('flag2').on(false));
-  //
-  //   expect(await queue.take()).toEqual('flag1');
-  //   expect(await queue.take()).toEqual('flag2');
-  // });
-  //
-  // it('sends an event when a segment used by a flag is updated', async () => {
-  //   const segment = {
-  //     key: 'segment1',
-  //     includedContexts: [{ contextKind: 'org', values: ['org-key'] }],
-  //     version: 1,
-  //   };
-  //
-  //   td.usePreconfiguredSegment(segment);
-  //   td.usePreconfiguredFlag(makeFlagWithSegmentMatch(segment));
-  //
-  //   td.usePreconfiguredSegment({
-  //     key: 'segment1',
-  //     includedContexts: [{ contextKind: 'org', values: ['org-key', 'second-key'] }],
-  //     version: 2,
-  //   });
-  //
-  //   expect(await queue.take()).toEqual('feature');
-  // });
-  //
-  // it('sends an event for a nested segment update', async () => {
-  //   const segment1 = {
-  //     key: 'segment1',
-  //     includedContexts: [{ contextKind: 'org', values: ['org-key'] }],
-  //     version: 1,
-  //   };
-  //   const segment2 = {
-  //     key: 'segment2',
-  //     rules: [
-  //       {
-  //         id: 'rule-1',
-  //         clauses: [
-  //           {
-  //             attribute: '',
-  //             op: 'segmentMatch' as Op,
-  //             values: [segment1.key],
-  //             attributeReference: new AttributeReference(''),
-  //           },
-  //         ],
-  //         weight: 100000,
-  //       },
-  //     ],
-  //     version: 1,
-  //   };
-  //   td.usePreconfiguredSegment(segment1);
-  //   td.usePreconfiguredSegment(segment2);
-  //   td.usePreconfiguredFlag(makeFlagWithSegmentMatch(segment2));
-  //
-  //   td.usePreconfiguredSegment({
-  //     key: 'segment1',
-  //     includedContexts: [{ contextKind: 'org', values: ['org-key', 'second-key'] }],
-  //     version: 2,
-  //   });
-  //
-  //   expect(await queue.take()).toEqual('feature');
-  // });
-  //
-  // it('does not hang on circular segment dependencies', async () => {
-  //   const segment1 = {
-  //     key: 'segment1',
-  //     clauses: [{ attribute: '', op: 'segmentMatch', values: ['segment2'] }],
-  //     version: 1,
-  //   };
-  //   const segment2 = {
-  //     key: 'segment2',
-  //     rules: [
-  //       {
-  //         id: 'rule-1',
-  //         clauses: [
-  //           {
-  //             attribute: '',
-  //             op: 'segmentMatch' as Op,
-  //             values: [segment1.key],
-  //             attributeReference: new AttributeReference(''),
-  //           },
-  //         ],
-  //         weight: 100000,
-  //       },
-  //     ],
-  //     version: 1,
-  //   };
-  //
-  //   td.usePreconfiguredSegment(segment1);
-  //   td.usePreconfiguredSegment(segment2);
-  //   td.usePreconfiguredFlag(makeFlagWithSegmentMatch(segment2));
-  //
-  //   td.usePreconfiguredSegment({
-  //     key: 'segment1',
-  //     includedContexts: [{ contextKind: 'org', values: ['org-key', 'second-key'] }],
-  //     version: 2,
-  //   });
-  //
-  //   expect(await queue.take()).toEqual('feature');
-  // });
+  it('sends an event when a flag is updated', async () => {
+    td.update(td.flag('flag1').on(true));
+    td.update(td.flag('flag2').on(true));
+
+    expect(await queue.take()).toEqual('flag1');
+    expect(await queue.take()).toEqual('flag2');
+
+    td.update(td.flag('flag1').on(false));
+    td.update(td.flag('flag2').on(false));
+
+    expect(await queue.take()).toEqual('flag1');
+    expect(await queue.take()).toEqual('flag2');
+  });
+
+  it('sends an event when a segment used by a flag is updated', async () => {
+    const segment = {
+      key: 'segment1',
+      includedContexts: [{ contextKind: 'org', values: ['org-key'] }],
+      version: 1,
+    };
+
+    td.usePreconfiguredSegment(segment);
+    td.usePreconfiguredFlag(makeFlagWithSegmentMatch(segment));
+
+    td.usePreconfiguredSegment({
+      key: 'segment1',
+      includedContexts: [{ contextKind: 'org', values: ['org-key', 'second-key'] }],
+      version: 2,
+    });
+
+    expect(await queue.take()).toEqual('feature');
+  });
+
+  it('sends an event for a nested segment update', async () => {
+    const segment1 = {
+      key: 'segment1',
+      includedContexts: [{ contextKind: 'org', values: ['org-key'] }],
+      version: 1,
+    };
+    const segment2 = {
+      key: 'segment2',
+      rules: [
+        {
+          id: 'rule-1',
+          clauses: [
+            {
+              attribute: '',
+              op: 'segmentMatch' as Op,
+              values: [segment1.key],
+              attributeReference: new AttributeReference(''),
+            },
+          ],
+          weight: 100000,
+        },
+      ],
+      version: 1,
+    };
+    td.usePreconfiguredSegment(segment1);
+    td.usePreconfiguredSegment(segment2);
+    td.usePreconfiguredFlag(makeFlagWithSegmentMatch(segment2));
+
+    td.usePreconfiguredSegment({
+      key: 'segment1',
+      includedContexts: [{ contextKind: 'org', values: ['org-key', 'second-key'] }],
+      version: 2,
+    });
+
+    expect(await queue.take()).toEqual('feature');
+  });
+
+  it('does not hang on circular segment dependencies', async () => {
+    const segment1 = {
+      key: 'segment1',
+      clauses: [{ attribute: '', op: 'segmentMatch', values: ['segment2'] }],
+      version: 1,
+    };
+    const segment2 = {
+      key: 'segment2',
+      rules: [
+        {
+          id: 'rule-1',
+          clauses: [
+            {
+              attribute: '',
+              op: 'segmentMatch' as Op,
+              values: [segment1.key],
+              attributeReference: new AttributeReference(''),
+            },
+          ],
+          weight: 100000,
+        },
+      ],
+      version: 1,
+    };
+
+    td.usePreconfiguredSegment(segment1);
+    td.usePreconfiguredSegment(segment2);
+    td.usePreconfiguredFlag(makeFlagWithSegmentMatch(segment2));
+
+    td.usePreconfiguredSegment({
+      key: 'segment1',
+      includedContexts: [{ contextKind: 'org', values: ['org-key', 'second-key'] }],
+      version: 2,
+    });
+
+    expect(await queue.take()).toEqual('feature');
+  });
 });
