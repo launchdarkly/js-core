24878cbe524fd9fdec705096525d567912bdd38f
Yusinto Ngadiman <yusinto@gmail.com>
2023-09-21 00:19:28 -0700

    Delete link-dev.sh

--- packages/sdk/cloudflare/example/link-dev.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-rm -rf node_modules/@launchdarkly
-mkdir -p node_modules/@launchdarkly
-rsync -aq ../dist node_modules/@launchdarkly/cloudflare-server-sdk
-rsync -aq ../package.json node_modules/@launchdarkly/cloudflare-server-sdk
