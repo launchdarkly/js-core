7af4f6e2d029b87396087d96904cdfa7d39a8cb3
Yusinto Ngadiman <yusinto@gmail.com>
2023-09-21 13:09:46 -0700

    feat: rollup cloudflare sdk (#279)
    Bundle cloudflare sdk using rollup and in process remove js-core and
    package.json imports.

--- scripts/build-package.sh
+++ scripts/build-package.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 # Run this script like:
 # ./scripts/build-package.sh
 
@@ -23,7 +24,7 @@ ESM_PACKAGE_JSON=$( jq -n \
                   --arg type "module" \
                   '{ name: $name, version: $version, type: $type }' )
 
-tsc --module commonjs --outDir dist/cjs/ 
+tsc --module commonjs --outDir dist/cjs/
 echo "$CJS_PACKAGE_JSON" > dist/cjs/package.json
 
 tsc --module es2022 --outDir dist/esm/
