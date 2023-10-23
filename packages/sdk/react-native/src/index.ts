33b3bbb34aff3f99d10aa8f8b11539350ff93b64
Yusinto Ngadiman <yusinto@gmail.com>
2023-10-23 10:44:47 -0700

    chore: sort tsconfig options alphabetically. add initial draft docs comments.

--- packages/sdk/react-native/src/index.ts
+++ packages/sdk/react-native/src/index.ts
@@ -1,3 +1,12 @@
+/**
+ * This is the API reference for the React Native LaunchDarkly SDK.
+ *
+ * TODO:
+ *
+ * For more information, see the SDK reference guide.
+ *
+ * @packageDocumentation
+ */
 import init from './init';
 import { setupPolyfill } from './polyfills';
 
