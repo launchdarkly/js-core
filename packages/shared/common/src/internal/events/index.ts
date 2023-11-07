aae16d80a475f1af42eb1a2db6e4456d79c547b3
Yusinto Ngadiman <yusinto@gmail.com>
2023-11-07 12:46:54 -0800

    chore: resolve conflicts.

--- packages/shared/common/src/internal/events/index.ts
+++ packages/shared/common/src/internal/events/index.ts
@@ -5,9 +5,9 @@ import InputEvalEvent from './InputEvalEvent';
 import InputEvent from './InputEvent';
 import InputIdentifyEvent from './InputIdentifyEvent';
 import InputMigrationEvent from './InputMigrationEvent';
+import type { LDInternalOptions } from './LDInternalOptions';
 import NullEventProcessor from './NullEventProcessor';
 import shouldSample from './sampling';
-import type { LDInternalOptions } from './LDInternalOptions';
 
 export {
   ClientMessages,
