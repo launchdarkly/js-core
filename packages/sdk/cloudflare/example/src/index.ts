2acad7085393caf60542d01260d86387ab29e4ce
Yusinto Ngadiman <yusinto@gmail.com>
2023-11-07 13:05:01 -0800

    chore: in cloudflare example, rename sdkKey to clientSideID.

--- packages/sdk/cloudflare/example/src/index.ts
+++ packages/sdk/cloudflare/example/src/index.ts
@@ -3,7 +3,7 @@ import { init as initLD } from '@launchdarkly/cloudflare-server-sdk';
 
 export default {
   async fetch(request: Request, env: Bindings, ctx: ExecutionContext): Promise<Response> {
-    const sdkKey = 'test-sdk-key';
+    const clientSideID = 'test-client-side-id';
     const flagKey = 'testFlag1';
     const { searchParams } = new URL(request.url);
 
@@ -12,7 +12,7 @@ export default {
     const context = { kind: 'user', key: 'test-user-key-1', email };
 
     // start using ld
-    const client = initLD(sdkKey, env.LD_KV, { sendEvents: true });
+    const client = initLD(clientSideID, env.LD_KV, { sendEvents: true });
     await client.waitForInitialization();
     const flagValue = await client.variation(flagKey, context, false);
     const flagDetail = await client.variationDetail(flagKey, context, false);
