cfba59b372b6d964d7d3d73b375387641c6a0256
Yusinto Ngadiman <yusinto@gmail.com>
2023-11-30 10:32:47 -0800

    Update welcome.tsx

--- packages/sdk/react-native/example/src/welcome.tsx
+++ packages/sdk/react-native/example/src/welcome.tsx
@@ -1,18 +1,26 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
-import { useBoolVariation, useLDDataSourceStatus } from '@launchdarkly/react-native-client-sdk';
+import {
+  useBoolVariation,
+  useLDClient,
+  useLDDataSourceStatus,
+} from '@launchdarkly/react-native-client-sdk';
 
 export default function Welcome() {
   const { error, status } = useLDDataSourceStatus();
   const flag = useBoolVariation('dev-test-flag', false);
-  console.log(`============== status: ${status}`);
+  const ldc = useLDClient();
+  const login = () => {
+    ldc.identify({ kind: 'user', key: 'test-user-2' });
+  };
 
   return (
     <View style={styles.container}>
       <Text>Welcome to LaunchDarkly</Text>
-      <Text>status: {status}</Text>
-      <Text>error: {error?.message}</Text>
+      <Text>status: {status ?? 'not connected'}</Text>
+      {error ? <Text>error: {error.message}</Text> : null}
       <Text>devTestFlag: {`${flag}`}</Text>
+      <Button title="Login" onPress={login} />
     </View>
   );
 }
