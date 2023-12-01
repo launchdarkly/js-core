b049c490fb190fbe66918b76de725baa5fd15901
Yusinto Ngadiman <yusinto@gmail.com>
2023-12-01 13:19:26 -0800

    chore: use console.log as custom logger to remove annoying error messages in dev.

--- packages/sdk/react-native/src/ReactNativeLDClient.ts
+++ packages/sdk/react-native/src/ReactNativeLDClient.ts
@@ -1,5 +1,6 @@
 import {
   base64UrlEncode,
+  BasicLogger,
   LDClientImpl,
   type LDContext,
   type LDOptions,
@@ -9,7 +10,14 @@ import platform from './platform';
 
 export default class ReactNativeLDClient extends LDClientImpl {
   constructor(sdkKey: string, options: LDOptions = {}) {
-    super(sdkKey, platform, options);
+    const logger =
+      options.logger ??
+      new BasicLogger({
+        level: 'info',
+        // eslint-disable-next-line no-console
+        destination: console.log,
+      });
+    super(sdkKey, platform, { ...options, logger });
   }
 
   override createStreamUriPath(context: LDContext) {
