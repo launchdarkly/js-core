02f5c36273d1c8b6d1e0878f214e3cd5d66d5b90
Yusinto Ngadiman <yusinto@gmail.com>
2023-12-18 15:53:26 -0800

    chore: added comments on how to install async-storage.

--- packages/sdk/react-native/src/platform.ts
+++ packages/sdk/react-native/src/platform.ts
@@ -1,4 +1,18 @@
 /* eslint-disable max-classes-per-file */
+
+/**
+ * The LaunchDarkly SDK uses async-storage for bootstrapping and this is a native
+ * dependency.
+ *
+ * If you are using expo, then adding the LaunchDarkly React Native
+ * SDK from npm and re-running pod install should suffice.
+ *
+ * If you are not using expo, you will need to explicitly add
+ * @react-native-async-storage/async-storage as a dependency to your project
+ * and re-run pod install for auto-linking to work. This is because auto-link
+ * does not work with transitive dependencies:
+ * https://github.com/react-native-community/cli/issues/1347
+ */
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 // @ts-ignore
