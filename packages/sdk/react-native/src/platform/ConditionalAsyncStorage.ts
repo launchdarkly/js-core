8e9a0c19d970dd85a50ea1e0a3ca2010b67ace37
Yusinto Ngadiman <yusinto@gmail.com>
2023-12-20 15:49:27 -0800

    chore: improve comments.

--- packages/sdk/react-native/src/platform/ConditionalAsyncStorage.ts
+++ packages/sdk/react-native/src/platform/ConditionalAsyncStorage.ts
@@ -1,29 +1,31 @@
-import rn from 'react-native';
+/* eslint-disable import/no-mutable-exports,global-require */
 
-// eslint-disable-next-line import/no-mutable-exports
+/**
+ * For react-native version >= 0.71, the LaunchDarkly React-Native SDK uses
+ * @react-native-async-storage/async-storage for bootstrapping. This is a native
+ * dependency.
+ *
+ * If you are using expo, then adding the LaunchDarkly React Native
+ * SDK from npm and re-running pod install should suffice.
+ *
+ * If you are not using expo, you will need to explicitly add
+ * @react-native-async-storage/async-storage as a dependency to your project
+ * and re-run pod install for auto-linking to work. This is because auto-link
+ * does not work with transitive dependencies:
+ * https://github.com/react-native-community/cli/issues/1347
+ *
+ * For react-native version < 0.71, the built-in react-native AsyncStorage
+ * module is used.
+ */
 let ConditionalAsyncStorage: any;
 
 try {
-  // @ts-ignore
-  ConditionalAsyncStorage = rn.AsyncStorage;
+  // react-native version < 0.71
+  ConditionalAsyncStorage = require('react-native').AsyncStorage;
   console.log('============ Native AsyncStorage');
 } catch (e) {
+  // react-native version >= 0.71
   console.log('============ @react-native-async-storage/async-storage');
-  /**
-   * The LaunchDarkly SDK uses async-storage for bootstrapping and this is a native
-   * dependency.
-   *
-   * If you are using expo, then adding the LaunchDarkly React Native
-   * SDK from npm and re-running pod install should suffice.
-   *
-   * If you are not using expo, you will need to explicitly add
-   * @react-native-async-storage/async-storage as a dependency to your project
-   * and re-run pod install for auto-linking to work. This is because auto-link
-   * does not work with transitive dependencies:
-   * https://github.com/react-native-community/cli/issues/1347
-   */
-  // @ts-ignore
-  // eslint-disable-next-line global-require
   ConditionalAsyncStorage = require('@react-native-async-storage/async-storage').default;
 }
 
