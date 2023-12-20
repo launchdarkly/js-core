4fed6301e37d652d496fb118f91b4c3263f39652
Yusinto Ngadiman <yusinto@gmail.com>
2023-12-20 10:53:13 -0800

    Update LDClientImpl.ts

--- packages/shared/sdk-client/src/LDClientImpl.ts
+++ packages/shared/sdk-client/src/LDClientImpl.ts
@@ -229,7 +229,7 @@ export default class LDClientImpl implements LDClient {
     }
 
     const { identifyPromise, identifyResolve } = this.createPromiseWithListeners();
-    this.logger.debug(`Identifying ${context}`);
+    this.logger.debug(`Identifying ${JSON.stringify(context)}`);
     this.emitter.emit('identifying', context);
 
     const flagsStorage = await this.getFlagsFromStorage(checkedContext.canonicalKey);
