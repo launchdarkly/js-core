56f18474786833b95f0935f9cdad7919f85f9433
Yusinto Ngadiman <yusinto@gmail.com>
2023-12-28 22:17:21 -0800

    chore: added more provider tests.

--- packages/sdk/react-native/src/provider/LDProvider.test.tsx
+++ packages/sdk/react-native/src/provider/LDProvider.test.tsx
@@ -1,26 +1,14 @@
-import { act, render } from '@testing-library/react';
+import { render } from '@testing-library/react';
 
 import type { LDContext, LDOptions } from '@launchdarkly/js-client-sdk-common';
 
 import { useLDClient } from '../hooks';
 import ReactNativeLDClient from '../ReactNativeLDClient';
 import LDProvider from './LDProvider';
+import setupListeners from './setupListeners';
 
-jest.mock('../ReactNativeLDClient', () =>
-  jest.fn((mobileKey: string, _options: LDOptions) => {
-    let context: LDContext;
-
-    return {
-      sdkKey: mobileKey,
-      identify: jest.fn((c: LDContext) => {
-        context = c;
-        return Promise.resolve();
-      }),
-      getContext: jest.fn(() => context),
-      on: jest.fn(),
-    };
-  }),
-);
+jest.mock('./setupListeners');
+jest.mock('../ReactNativeLDClient', () => jest.fn());
 
 const TestApp = () => {
   const ldClient = useLDClient();
@@ -33,12 +21,39 @@ const TestApp = () => {
   );
 };
 describe('LDProvider', () => {
-  let ldc: ReactNativeLDClient;
+  let ldc: any;
   let context: LDContext;
+  let mockSetupListeners = setupListeners as jest.Mock;
 
   beforeEach(() => {
-    ldc = new ReactNativeLDClient('mobile-key', { sendEvents: false });
+    jest.useFakeTimers();
+    (ReactNativeLDClient as jest.Mock).mockImplementation(
+      (mobileKey: string, _options?: LDOptions) => {
+        let context: LDContext;
+
+        return {
+          sdkKey: mobileKey,
+          identify: jest.fn((c: LDContext) => {
+            context = c;
+            return Promise.resolve();
+          }),
+          getContext: jest.fn(() => context),
+          on: jest.fn(),
+          logger: {
+            debug: jest.fn(),
+          },
+        };
+      },
+    );
+    mockSetupListeners.mockImplementation((client: ReactNativeLDClient, setState: any) => {
+      setState({ client });
+    });
+    ldc = new ReactNativeLDClient('mobile-key');
     context = { kind: 'user', key: 'test-user-key-1' };
+  });
+
+  afterEach(() => {
+    jest.resetAllMocks();
   });
 
   test('client is correctly set', () => {
@@ -53,18 +68,30 @@ describe('LDProvider', () => {
     expect(getByText(/context undefined/i)).toBeTruthy();
   });
 
-  test.only('specified context is identified', async () => {
-    let output;
-    await act(async () => {
-      output = render(
-        <LDProvider client={ldc} context={context}>
-          <TestApp />
-        </LDProvider>,
-      );
-    });
+  test('specified context is identified', async () => {
+    const { getByText } = render(
+      <LDProvider client={ldc} context={context}>
+        <TestApp />
+      </LDProvider>,
+    );
 
-    expect(output!.getByText(/context defined/i)).toBeTruthy();
+    expect(mockSetupListeners).toHaveBeenCalledWith(ldc, expect.any(Function));
+    expect(ldc.identify).toHaveBeenCalledWith(context);
+    expect(ldc.getContext()).toEqual(context);
+    expect(getByText(/context defined/i)).toBeTruthy();
   });
 
-  test.todo('listeners are setup correctly');
+  test('identify errors are caught', async () => {
+    (ldc.identify as jest.Mock).mockImplementation(() => {
+      return Promise.reject('faking error when identifying');
+    });
+    const { getByText } = render(
+      <LDProvider client={ldc} context={context}>
+        <TestApp />
+      </LDProvider>,
+    );
+    await jest.runAllTimersAsync();
+
+    expect(ldc.logger.debug).toHaveBeenCalledWith(expect.stringMatching(/identify error/));
+  });
 });
