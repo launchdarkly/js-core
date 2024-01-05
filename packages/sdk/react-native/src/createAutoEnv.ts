7bf8e043123822639797ac04abe4c08a1d054828
Yusinto Ngadiman <yusinto@gmail.com>
2024-01-05 12:07:56 -0800

    Update createAutoEnv.ts

--- packages/sdk/react-native/src/createAutoEnv.ts
+++ packages/sdk/react-native/src/createAutoEnv.ts
@@ -4,6 +4,13 @@ export default function createAutoEnv(): LDAutoEnv {
   // TODO: populate env fields correctly
   return {
     ld_application: {
+      /**
+       * Priority for id, version and versionName:
+       *
+       * 1. Customer provided values via configuration or provided otherwise
+       * 2. Application info collected from the platform via platform APIs
+       * 3. LaunchDarkly SDK info such as SDK name and version
+       */
       key: '',
       envAttributesVersion: '',
       id: '',
