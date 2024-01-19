c3883a36bfccb2a475f8b7134dec9ed2a1c68dbc
Yusinto Ngadiman <yusinto@gmail.com>
2024-01-19 15:04:06 -0800

    chore: Refactored internal inject functions.

--- packages/shared/sdk-client/src/utils/injectAutoEnv.ts
+++ packages/shared/sdk-client/src/utils/injectAutoEnv.ts
@@ -25,21 +25,9 @@ const toMulti = (c: LDSingleKindContext) => {
   };
 };
 
-const injectAutoEnv = async (context: LDContext, platform: Platform, config: Configuration) => {
-  // LDUser is not supported for auto env reporting
-  if (isLegacyUser(context)) {
-    return context as LDUser;
-  }
-
-  let multi;
-  const cloned = clone<LDContext>(context);
-  if (isSingleKind(cloned)) {
-    multi = toMulti(cloned);
-  }
-
-  const { crypto, info } = platform;
+const injectApplication = ({ crypto, info }: Platform, config: Configuration) => {
   const { name, version, wrapperName, wrapperVersion } = info.sdkData();
-  const { ld_application, ld_device } = info.platformData();
+  const { ld_application } = info.platformData();
 
   const ldApplication = clone<LDApplication>(ld_application);
   ldApplication.id = config.application?.id ?? ldApplication.id ?? name ?? wrapperName;
@@ -50,12 +38,33 @@ const injectAutoEnv = async (context: LDContext, platform: Platform, config: Con
   hasher.update(ldApplication.id!);
   ldApplication.key = hasher.digest('base64');
 
-  const ldDevice = clone<LDDevice>(ld_device);
-  if (ld_device && !ld_device.key) {
+  return ldApplication;
+};
+
+const injectDevice = async (platform: Platform) => {
+  const ldDevice = clone<LDDevice>(platform.info.platformData().ld_device);
+  if (ldDevice && !ldDevice.key) {
     ldDevice.key = await getOrGenerateKey('ld_device', platform);
   }
+  return ldDevice;
+};
 
-  return { ...multi, ld_application, ld_device } as LDMultiKindContext;
+const injectAutoEnv = async (context: LDContext, platform: Platform, config: Configuration) => {
+  // LDUser is not supported for auto env reporting
+  if (isLegacyUser(context)) {
+    return context as LDUser;
+  }
+
+  let multi;
+  if (isSingleKind(context)) {
+    multi = toMulti(context);
+  }
+
+  return {
+    ...multi,
+    ld_application: injectApplication(platform, config),
+    ld_device: await injectDevice(platform),
+  } as LDMultiKindContext;
 };
 
 export default injectAutoEnv;
