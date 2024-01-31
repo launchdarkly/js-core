a2395c5eb0da7c98b681a0e40f7403f35f561890
Yusinto Ngadiman <yusinto@gmail.com>
2024-01-31 14:08:40 -0800

    chore: Add debounce function to common utils.

---
+++ packages/shared/common/src/utils/debounce.ts
@@ -0,0 +1,41 @@
+/**
+ * Wait before calling the same function. Useful for expensive calls.
+ *
+ * Adapted from:
+ * https://amitd.co/code/typescript/debounce
+ *
+ *
+ * @return The debounced function.
+ *
+ * @example
+ *
+ * ```js
+ * const debouncedFunction = debounce(e => {
+ *   console.log(e);
+ * }, 5000);
+ *
+ * // Console logs 'Hello world again ' after 5 seconds
+ * debouncedFunction('Hello world');
+ * debouncedFunction('Hello world again');
+ * ```
+ * @param fn
+ * @param delayMs
+ */
+const debounce = <T extends (...args: any[]) => ReturnType<T>>(
+  fn?: T,
+  delayMs: number = 5000,
+): ((...args: Parameters<T>) => void) | undefined => {
+  if (!fn) {
+    return fn;
+  }
+
+  let timer: ReturnType<typeof setTimeout>;
+
+  return (...args: Parameters<T>) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      fn(...args);
+    }, delayMs);
+  };
+};
+export default debounce;
