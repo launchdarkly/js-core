f5a60905b64b788fe6e2d05b999d87455ef80166
Yusinto Ngadiman <yusinto@gmail.com>
2024-02-01 11:07:52 -0800

    fix: Attempt to fix "jest/globals" is unknown.

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,7 @@ module.exports = {
   parserOptions: {
     project: './tsconfig.eslint.json',
   },
-  plugins: ['@typescript-eslint', 'prettier', 'jest'],
+  plugins: ['jest', '@typescript-eslint', 'prettier'],
   ignorePatterns: [
     '**/dist/**',
     '**/vercel/examples/**',
