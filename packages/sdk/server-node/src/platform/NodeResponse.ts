030bd44496033b9ce5f0abaf56daea3686330b48
Ryan Lamb <4955475+kinyoklion@users.noreply.github.com>
2024-02-06 16:09:17 -0800

    feat: Implement handling for gzip compressed responses.

--- packages/sdk/server-node/src/platform/NodeResponse.ts
+++ packages/sdk/server-node/src/platform/NodeResponse.ts
@@ -1,13 +1,41 @@
 import * as http from 'http';
+import * as zlib from 'zlib';
+import { pipeline, Writable } from 'stream';
 
 import { platform } from '@launchdarkly/js-server-sdk-common';
 
 import HeaderWrapper from './HeaderWrapper';
 
+/**
+ * Memory stream for used to allow pipelining with decompression.
+ * 
+ * This implementation is not general purpose.
+ */
+class MemoryStream extends Writable {
+  private chunks: any[] = [];
+
+  override _write(chunk: any, _encoding: BufferEncoding, _onError: (error?: Error | null) => void) {
+    this.chunks.push(chunk);
+  }
+
+  override toString() {
+    return Buffer.concat(this.chunks).toString()
+  }
+
+  override end(cb?: (() => void) | undefined): this;
+  override end(chunk: any, cb?: (() => void) | undefined): this;
+  override end(chunk: any, encoding: BufferEncoding, cb?: (() => void) | undefined): this;
+  override end(chunk?: unknown, encoding?: unknown, cb?: unknown): this {
+    this._write(chunk, encoding as BufferEncoding, cb as (error?: Error | null) => void);
+    this.emit('finish');
+    return this;
+  }
+}
+
 export default class NodeResponse implements platform.Response {
   incomingMessage: http.IncomingMessage;
 
-  body: any[] = [];
+  memoryStream: MemoryStream = new MemoryStream();
 
   promise: Promise<string>;
 
@@ -22,18 +50,24 @@ export default class NodeResponse implements platform.Response {
     this.status = res.statusCode || 0;
     this.incomingMessage = res;
 
+
+
     this.promise = new Promise((resolve, reject) => {
-      res.on('data', (chunk) => {
-        this.body.push(chunk);
-      });
-
-      res.on('error', (err) => {
-        reject(err);
-      });
-
-      res.on('end', () => {
-        resolve(Buffer.concat(this.body).toString());
-      });
+      // Called on error or completion of the pipeline.
+      const pipelineCallback = (err: any) => {
+        if(err) {
+          reject(err);
+        }
+        resolve(this.memoryStream.toString());
+    };
+      switch (res.headers['content-encoding']) {
+        case 'gzip':
+          pipeline(res, zlib.createGunzip(), this.memoryStream, pipelineCallback);
+          break;
+        default:
+          pipeline(res, this.memoryStream, pipelineCallback);
+          break;
+      }
     });
   }
 
