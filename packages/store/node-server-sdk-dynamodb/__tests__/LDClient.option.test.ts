8e67c651994a1bea7aa7d1bddd3a71b52587b7ab
Ryan Lamb <4955475+kinyoklion@users.noreply.github.com>
2024-03-29 11:51:02 -0700

    Lint

--- packages/store/node-server-sdk-dynamodb/__tests__/LDClient.option.test.ts
+++ packages/store/node-server-sdk-dynamodb/__tests__/LDClient.option.test.ts
@@ -1,11 +1,13 @@
 import { LDOptions } from '@launchdarkly/node-server-sdk';
+
 import DynamoDBBigSegmentStoreFactory from '../src/DynamoDBBigSegmentStoreFactory';
 
 it('can construct options with a big segment store', () => {
   // This is just a typescript test to ensure the typings match.
-  const _: LDOptions = {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const options: LDOptions = {
     bigSegments: {
-      store: DynamoDBBigSegmentStoreFactory("my-table")
-    }
-  }
+      store: DynamoDBBigSegmentStoreFactory('my-table'),
+    },
+  };
 });
--- packages/store/node-server-sdk-redis/__tests__/LDClient.option.test.ts
+++ packages/store/node-server-sdk-redis/__tests__/LDClient.option.test.ts
@@ -1,11 +1,12 @@
 import { LDOptions } from '@launchdarkly/node-server-sdk';
+
 import RedisBigSegmentStoreFactory from '../src/RedisBigSegmentStoreFactory';
 
 it('can construct options with a big segment store', () => {
-  // This is just a typescript test to ensure the typings match.
-  const _: LDOptions = {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const options: LDOptions = {
     bigSegments: {
-      store: RedisBigSegmentStoreFactory()
-    }
-  }
+      store: RedisBigSegmentStoreFactory(),
+    },
+  };
 });
