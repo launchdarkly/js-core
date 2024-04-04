eba02cf8eda815f6bbe79fa08b40b1243a101e42
Ryan Lamb <4955475+kinyoklion@users.noreply.github.com>
2024-04-04 09:27:38 -0700

    feat: Add OpenTelemetry support for node-server-sdk. (#401)

---
+++ packages/telemetry/node-server-sdk-otel/jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  transform: { '^.+\\.ts?$': 'ts-jest' },
+  testMatch: ['**/*.test.ts?(x)'],
+  testEnvironment: 'node',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  collectCoverageFrom: ['src/**/*.ts'],
+  setupFilesAfterEnv: ['@launchdarkly/private-js-mocks/setup'],
+};
