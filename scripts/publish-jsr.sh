920b848a0521eeb201e679ae808ea589e18bfc19
Yusinto Ngadiman <yusinto@gmail.com>
2024-04-15 12:22:47 -0700

    chore: Independent jsr publishing scripts.

---
+++ scripts/publish-jsr.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+if [ -f "./$WORKSPACE_PATH/jsr.json" ]; then
+  cd $WORKSPACE_PATH
+  
+  if $LD_RELEASE_IS_DRYRUN ; then
+    echo "Doing a dry run of jsr publishing."
+    npx jsr publish --dry-run || { echo "jsr publish failed" >&2; exit 1; }
+  elif [ -f "./$WORKSPACE_PATH/jsr.json" ]; then
+    echo "Publishing to jsr."
+    npx jsr publish || { echo "jsr publish failed" >&2; exit 1; }
+  fi
+fi
--- scripts/publish.sh
+++ scripts/publish.sh
@@ -8,13 +8,6 @@ else
     echo "Publishing with prerelease tag."
     npm publish --tag prerelease --provenance --access public "./$WORKSPACE_PATH/package.tgz" || { echo "npm publish failed" >&2; exit 1; }
   else
-    if [ -f "./$WORKSPACE_PATH/jsr.json" ]; then
-      echo "Publishing to jsr."
-      cd $WORKSPACE_PATH
-      npx jsr publish || { echo "jsr publish failed" >&2; exit 1; }
-    fi
-        
-    echo "Publishing to npm."
     npm publish --provenance --access public "./$WORKSPACE_PATH/package.tgz" || { echo "npm publish failed" >&2; exit 1; }
   fi
 fi
