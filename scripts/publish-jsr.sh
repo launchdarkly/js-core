5a17f81b62b141e020ecd96feb430063d84709ac
Yusinto Ngadiman <yusinto@gmail.com>
2024-04-22 13:58:11 -0700

    fix: Allow dirty for jsr publishing.

--- scripts/publish-jsr.sh
+++ scripts/publish-jsr.sh
@@ -5,10 +5,10 @@ if [ -f "./$WORKSPACE_PATH/jsr.json" ]; then
   git diff HEAD
   if $LD_RELEASE_IS_DRYRUN ; then
     echo "Doing a dry run of jsr publishing."
-    npx jsr publish --dry-run || { echo "jsr publish failed" >&2; exit 1; }
+    npx jsr publish --dry-run --allow-dirty || { echo "jsr publish failed" >&2; exit 1; }
   elif [ -f "./$WORKSPACE_PATH/jsr.json" ]; then
     echo "Publishing to jsr."
-    npx jsr publish || { echo "jsr publish failed" >&2; exit 1; }
+    npx jsr publish --allow-dirty || { echo "jsr publish failed" >&2; exit 1; }
   fi
 else
   echo "Skipping jsr."
