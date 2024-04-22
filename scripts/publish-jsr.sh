9655073a0ba0a080d649a43be89d5c5571bde414
Yusinto Ngadiman <yusinto@gmail.com>
2024-04-22 14:00:02 -0700

    Update publish-jsr.sh

--- scripts/publish-jsr.sh
+++ scripts/publish-jsr.sh
@@ -2,7 +2,7 @@
 
 if [ -f "./$WORKSPACE_PATH/jsr.json" ]; then
   cd $WORKSPACE_PATH
-  git diff HEAD
+  
   if $LD_RELEASE_IS_DRYRUN ; then
     echo "Doing a dry run of jsr publishing."
     npx jsr publish --dry-run --allow-dirty || { echo "jsr publish failed" >&2; exit 1; }
