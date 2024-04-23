4666a09c339c1109235d09765bd7f1f39c1c9410
Yusinto Ngadiman <yusinto@gmail.com>
2024-04-22 22:25:11 -0700

    fix: Remove erroneous jsr.json check.

--- scripts/publish-jsr.sh
+++ scripts/publish-jsr.sh
@@ -6,9 +6,10 @@ if [ -f "./$WORKSPACE_PATH/jsr.json" ]; then
   if $LD_RELEASE_IS_DRYRUN ; then
     echo "Doing a dry run of jsr publishing."
     npx jsr publish --dry-run --allow-dirty || { echo "jsr publish failed" >&2; exit 1; }
-  elif [ -f "./$WORKSPACE_PATH/jsr.json" ]; then
-    echo "Publishing to jsr."
+  else
+    echo "Publishing to jsr.."
     npx jsr publish --allow-dirty || { echo "jsr publish failed" >&2; exit 1; }
+    echo "Successfully published to jsr."
   fi
 else
   echo "Skipping jsr."
