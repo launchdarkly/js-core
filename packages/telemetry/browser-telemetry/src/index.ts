9ce15af43017634fdf4de120ab67e97bdf682d64
Ryan Lamb <4955475+kinyoklion@users.noreply.github.com>
2024-06-20 15:23:55 -0700

    cleanup

--- packages/telemetry/browser-telemetry/src/index.ts
+++ packages/telemetry/browser-telemetry/src/index.ts
@@ -1,9 +1,6 @@
 import { BrowserTelemetry } from './api/BrowserTelemetry.js';
-import { LDClient, LDInspection } from 'launchdarkly-js-client-sdk';
-import { Collector } from './api/Collector.js';
 import { Options } from './api/Options.js';
 import BrowserTelemetryImpl from './BrowserTelemetryImpl.js';
-import ClickCollector from './collectors/click.js';
 import parse from './options.js';
 
 export { type Breadcrumb } from './api/Breadcrumb.js';
