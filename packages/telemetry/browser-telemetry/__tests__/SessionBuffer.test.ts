0d5e5ae7f111b5a2ced0ecee6cdd0d0934439f58
Ryan Lamb <4955475+kinyoklion@users.noreply.github.com>
2024-06-25 08:34:08 -0700

    feat: Basic session replay prototype. (#486)
    These are prototype changes targeting a prototype branch.
    
    Adds the ability to capture sessions as a custom event using rrweb.

---
+++ packages/telemetry/browser-telemetry/__tests__/SessionBuffer.test.ts
@@ -0,0 +1,29 @@
+import SessionBuffer from '../src/collectors/rrweb/SessionBuffer';
+
+it('can fill the entire expected buffer size', () => {
+  const bufferSize = 5;
+  const numberBuffers = 4;
+  const buffer = new SessionBuffer(bufferSize, numberBuffers);
+  const demoItems = Array.from(new Array(bufferSize * numberBuffers), (_, i) => i);
+
+  demoItems.forEach(buffer.push.bind(buffer));
+
+  expect(buffer.toArray()).toEqual(demoItems);
+});
+
+it('when the buffer is exceeded it will wrap around', () => {
+  const bufferSize = 5;
+  const numberBuffers = 4;
+  const buffer = new SessionBuffer(bufferSize, numberBuffers);
+  const dropRatio = 1.5;
+  const extraItems = Math.trunc(bufferSize * dropRatio);
+  const itemsToMake = bufferSize * numberBuffers + extraItems;
+  const demoItems = Array.from(new Array(itemsToMake), (_, i) => i);
+
+  demoItems.forEach(buffer.push.bind(buffer));
+
+  // We need to remove the number of chunks, not the specific number of items.
+  const expectedItems = demoItems.slice(Math.ceil(dropRatio) * bufferSize);
+
+  expect(buffer.toArray()).toEqual(expectedItems);
+});
---
+++ packages/telemetry/browser-telemetry/src/collectors/rrweb/SessionReplay.ts
@@ -0,0 +1,41 @@
+import * as rrweb from 'rrweb';
+
+import { BrowserTelemetry } from '../../api/BrowserTelemetry';
+import { Collector } from '../../api/Collector';
+import applyPatches from './patches';
+import SessionBuffer from './SessionBuffer';
+
+/**
+ * Experimental capture of sessions using rrweb.
+ */
+export default class SessionReplay implements Collector {
+  telemetry?: BrowserTelemetry;
+  buffer: SessionBuffer;
+
+  constructor(checkoutEveryNth: number, numBuffers: number) {
+    applyPatches();
+    this.buffer = new SessionBuffer(checkoutEveryNth, numBuffers);
+    const { buffer } = this;
+    rrweb.record({
+      checkoutEveryNth,
+      emit(event) {
+        buffer.push(event);
+      },
+    });
+  }
+
+  register(telemetry: BrowserTelemetry): void {
+    this.telemetry = telemetry;
+  }
+
+  unregister(): void {
+    this.telemetry = undefined;
+  }
+
+  capture(): void {
+    this.telemetry?.captureSession({
+      id: crypto.randomUUID(),
+      events: this.buffer.toArray(),
+    });
+  }
+}
