8b9ff9894401d729065938308e073bae5d0714b7
Yusinto Ngadiman <yusinto@gmail.com>
2024-06-27 16:18:08 -0700

    chore: Rolled back client component. Moved identify example to its own component.

--- packages/sdk/react-universal/example/app/components/helloClientComponent.tsx
+++ packages/sdk/react-universal/example/app/components/helloClientComponent.tsx
@@ -1,29 +1,15 @@
 'use client';
 
-import { useState } from 'react';
-import { useCookies } from 'react-cookie';
-
-import type { JSSdk } from '@launchdarkly/react-universal-sdk';
 import { useLDClient } from '@launchdarkly/react-universal-sdk/client';
 
 export default function HelloClientComponent() {
   const ldc = useLDClient();
-  const [_, setCookie] = useCookies(['ld']);
-  const [contextKey, setContextKey] = useState('');
 
   // WARNING: Using the ldClient to evaluate flags directly like this in prod
   // can result in high event volumes. This example is contrived and is meant for
   // demo purposes only. The recommended way is to utilise the `useVariation` hooks
   // which should be supported soon.
   const flagValue = ldc.variation('my-boolean-flag-1');
-
-  function onClickLogin() {
-    const context = { kind: 'user', key: contextKey };
-    (ldc as JSSdk).identify(context).then(() => {
-      console.log('identify successful, persisting to cookies');
-      setCookie('ld', context);
-    });
-  }
 
   return (
     <div className="border-2 border-white/20  p-4 ">
@@ -34,17 +20,6 @@ export default function HelloClientComponent() {
             : 'This flag is evaluating False running Client-Side JavaScript'}
         </p>
       </div>
-      <br />
-      <div>
-        <label>
-          Context key: &nbsp;
-          <input value={contextKey} onChange={(e) => setContextKey(e.target.value)} />
-        </label>
-      </div>
-      <br />
-      <button role="button" className="login" onClick={onClickLogin}>
-        Login
-      </button>
     </div>
   );
 }
