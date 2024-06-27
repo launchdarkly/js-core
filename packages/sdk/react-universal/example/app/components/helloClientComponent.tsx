4419ec0403f46d422765b1184e052813397e011e
Yusinto Ngadiman <yusinto@gmail.com>
2024-06-27 15:31:49 -0700

    chore: Added login input and improved button styles.

--- packages/sdk/react-universal/example/app/components/helloClientComponent.tsx
+++ packages/sdk/react-universal/example/app/components/helloClientComponent.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { useState } from 'react';
 import { useCookies } from 'react-cookie';
 
 import type { JSSdk } from '@launchdarkly/react-universal-sdk';
@@ -8,11 +9,12 @@ import { useLDClient } from '@launchdarkly/react-universal-sdk/client';
 export default function HelloClientComponent() {
   const ldc = useLDClient();
   const [_, setCookie] = useCookies(['ld']);
+  const [firstName, setFirstName] = useState('');
 
   const flagValue = ldc.variation('my-boolean-flag-1');
 
   function onClickLogin() {
-    const context = { kind: 'user', key: 'test-user-2' };
+    const context = { kind: 'user', key: firstName };
     (ldc as JSSdk).identify(context).then(() => {
       console.log('identify successful, persisting to cookies');
       setCookie('ld', context);
@@ -21,12 +23,24 @@ export default function HelloClientComponent() {
 
   return (
     <div className="border-2 border-white/20  p-4 ">
-      <p className="ldgradient text-xl">
-        {flagValue
-          ? 'This flag is evaluating True running Client-Side JavaScript'
-          : 'This flag is evaluating False running Client-Side JavaScript'}
-      </p>
-      <button onClick={onClickLogin}>Login</button>
+      <div>
+        <p className="ldgradient text-xl">
+          {flagValue
+            ? 'This flag is evaluating True running Client-Side JavaScript'
+            : 'This flag is evaluating False running Client-Side JavaScript'}
+        </p>
+      </div>
+      <br />
+      <div>
+        <label>
+          First name: &nbsp;
+          <input value={firstName} onChange={(e) => setFirstName(e.target.value)} />
+        </label>
+      </div>
+      <br />
+      <button role="button" className="login" onClick={onClickLogin}>
+        Login
+      </button>
     </div>
   );
 }
