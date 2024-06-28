e3191332136836708b8a47b954ed35a61dbe53e9
Yusinto Ngadiman <yusinto@gmail.com>
2024-06-28 09:41:58 -0700

    chore: Added minor comment.

--- packages/sdk/react-universal/src/client/hooks/useLDClient.ts
+++ packages/sdk/react-universal/src/client/hooks/useLDClient.ts
@@ -10,5 +10,6 @@ export const useLDClient = () => {
   const { context, bootstrap, jsSdk } = useContext<ReactContext>(reactContext);
 
   // TODO: memo construction of LDClientRsc
+  // On the browser, always use the js sdk.
   return jsSdk ?? new LDClientRsc(context, bootstrap);
 };
