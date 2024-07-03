131c1b4ada660b1c29d3109ca65ba530c3d48ebe
Yusinto Ngadiman <yusinto@gmail.com>
2024-07-03 12:46:02 -0700

    fix: Make defaults optional.

--- packages/sdk/react-universal/src/client/hooks/variation/useVariation.ts
+++ packages/sdk/react-universal/src/client/hooks/variation/useVariation.ts
@@ -1,7 +1,7 @@
 import { useTypedVariation, useTypedVariationDetail } from './useTypedVariation';
 
-export const useVariation = (key: string, defaultValue: boolean) =>
+export const useVariation = (key: string, defaultValue?: boolean) =>
   useTypedVariation<any>(key, defaultValue);
 
-export const useVariationDetail = (key: string, defaultValue: boolean) =>
+export const useVariationDetail = (key: string, defaultValue?: boolean) =>
   useTypedVariationDetail<any>(key, defaultValue);
--- packages/sdk/react-universal/src/server/hooks/variation/useVariationRsc.ts
+++ packages/sdk/react-universal/src/server/hooks/variation/useVariationRsc.ts
@@ -2,12 +2,12 @@ import type { LDContext, LDFlagValue } from '@launchdarkly/node-server-sdk';
 
 import { useLDClientRsc } from '../useLDClientRsc';
 
-export const useVariationRsc = async (key: string, context: LDContext, def: LDFlagValue) => {
+export const useVariationRsc = async (key: string, context: LDContext, def?: LDFlagValue) => {
   const ldc = await useLDClientRsc(context);
   return ldc.variation(key, def);
 };
 
-export const useVariationDetailRsc = async (key: string, context: LDContext, def: LDFlagValue) => {
+export const useVariationDetailRsc = async (key: string, context: LDContext, def?: LDFlagValue) => {
   const ldc = await useLDClientRsc(context);
   return ldc.variationDetail(key, def);
 };
