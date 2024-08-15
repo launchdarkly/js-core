448ad67815b9ec29abd322ed8483b2367147b146
Ryan Lamb <4955475+kinyoklion@users.noreply.github.com>
2024-08-15 12:11:57 -0700

    feat: Remove event target shim. (#545)
    Unlike the EventEmitter in the node SDK the EventTarget was not
    providing an interface directly on the LDClient. The LDClient has its
    own off/on interface which is not directly implemented via an event
    target. Which means removing it simplifies the implementation instead of
    complicating it.
    
    Second it is in the common code and node in a leaf implementation. Which
    means it requires a polyfill where it is not supported. Like when using
    hermes with React Native.
    
    Typically we dispatch using a micro-task, but EventTarget dispatches
    synchronously. For now this maintains the synchronous behavior.
    
    Fixes: #412

--- packages/sdk/react-native/src/polyfills/index.ts
+++ packages/sdk/react-native/src/polyfills/index.ts
@@ -1,13 +1,4 @@
-import EventTarget from 'event-target-shim';
-
 import { type Hasher, sha256 } from '../fromExternal/js-sha256';
 import { base64FromByteArray, btoa } from './btoa';
-import CustomEvent from './CustomEvent';
 
-function setupPolyfill() {
-  Object.assign(global, {
-    EventTarget,
-    CustomEvent,
-  });
-}
-export { base64FromByteArray, btoa, type Hasher, setupPolyfill, sha256 };
+export { base64FromByteArray, btoa, type Hasher, sha256 };
