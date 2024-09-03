840fa25684569d66ea8204f69646c399c42071f0
Ryan Lamb <4955475+kinyoklion@users.noreply.github.com>
2024-09-03 09:33:54 -0700

    fix: Conditionally access constructor name.

--- packages/telemetry/browser-telemetry/src/BrowserTelemetryImpl.ts
+++ packages/telemetry/browser-telemetry/src/BrowserTelemetryImpl.ts
@@ -134,10 +134,18 @@ export default class BrowserTelemetryImpl implements BrowserTelemetry {
   }
 
   captureError(exception: Error): void {
-    const data: ErrorData = {
-      type: exception.name || exception.constructor.name || 'generic',
+    const validException = exception !== undefined && exception !== null;
+
+    const data: ErrorData = validException ? {
+      type: exception.name || exception.constructor?.name || 'generic',
       message: exception.message,
       stack: parse(exception, this.options.stack),
+      breadcrumbs: [...this.breadcrumbs],
+      sessionId: this.sessionId,
+    } : {
+      type: 'generic',
+      message: 'null or undefined exception',
+      stack: { frames: [] },
       breadcrumbs: [...this.breadcrumbs],
       sessionId: this.sessionId,
     };
