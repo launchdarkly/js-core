232b7eee29be7056f74d9aa5989d893b729cc179
Ryan Lamb <4955475+kinyoklion@users.noreply.github.com>
2024-09-06 15:58:19 -0700

    fix tests

--- packages/sdk/browser/__tests__/platform/BrowserHasher.test.ts
+++ packages/sdk/browser/__tests__/platform/BrowserHasher.test.ts
@@ -34,20 +34,40 @@ describe('PlatformHasher', () => {
     expect(output).toEqual('5d59ba64d93a7a3c7af8856d2fbcdfc18c11436fdc93dfb2ec568ddf611cb9d4');
   });
 
+  test('sha1 produces correct base64 output', async () => {
+    // @ts-ignore
+    const h = new BrowserHasher(webcrypto, 'sha1');
+
+    h.update('test-app-id');
+    const output = await h.asyncDigest('base64');
+
+    expect(output).toEqual('kydC7cRd9+LWbu4Ss/t1FiFmDcs=');
+  });
+
+  test('sha1 produces correct hex output', async () => {
+    // @ts-ignore
+    const h = new BrowserHasher(webcrypto, 'sha1');
+
+    h.update('test-app-id');
+    const output = await h.asyncDigest('hex');
+
+    expect(output).toEqual('932742edc45df7e2d66eee12b3fb751621660dcb');
+  });
+
   test('unsupported hash algorithm', async () => {
     expect(() => {
       // @ts-ignore
       // eslint-disable-next-line no-new
-      new BrowserHasher(webcrypto, 'sha1');
-    }).toThrow(/unsupported/i);
+      new BrowserHasher(webcrypto, 'sha512');
+    }).toThrow(/Algorithm is not supported/i);
   });
 
   test('unsupported output algorithm', async () => {
-    expect(async () => {
+    await expect(async () => {
       // @ts-ignore
       const h = new BrowserHasher(webcrypto, 'sha256');
       h.update('test-app-id');
       await h.asyncDigest('base122');
-    }).toThrow(/unsupported/i);
+    }).rejects.toThrow(/Encoding is not supported/i);
   });
 });
