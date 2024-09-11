fd8d7d63e5e52646e57d521ea022683ed59542ec
Ryan Lamb <4955475+kinyoklion@users.noreply.github.com>
2024-09-11 14:39:42 -0700

    Validate package.json imports.

--- packages/sdk/browser/rollup.config.js
+++ packages/sdk/browser/rollup.config.js
@@ -2,7 +2,7 @@ import common from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import terser from '@rollup/plugin-terser';
 import typescript from '@rollup/plugin-typescript';
-import generatePackageJson from 'rollup-plugin-generate-package-json';
+import json from '@rollup/plugin-json';
 
 const getSharedConfig = (format, file) => ({
   input: 'src/index.ts',
@@ -24,13 +24,6 @@ export default [
   {
     ...getSharedConfig('es', 'dist/index.es.js'),
     plugins: [
-      generatePackageJson({
-        baseContents: (pkg) => ({
-          name: pkg.name,
-          version: pkg.version,
-          type: 'module',
-        }),
-      }),
       typescript({
         module: 'esnext',
       }),
@@ -40,22 +33,17 @@ export default [
       }),
       resolve(),
       terser(),
+      json(),
     ],
   },
   {
     ...getSharedConfig('cjs', 'dist/index.cjs.js'),
     plugins: [
-      generatePackageJson({
-        baseContents: (pkg) => ({
-          name: pkg.name,
-          version: pkg.version,
-          type: 'commonjs',
-        }),
-      }),
       typescript(),
       common(),
       resolve(),
       terser(),
+      json(),
     ],
   },
 ];
--- packages/sdk/browser/src/index.ts
+++ packages/sdk/browser/src/index.ts
@@ -1,7 +1,8 @@
+import BrowserInfo from './platform/BrowserInfo';
 import DefaultBrowserEventSource from './platform/DefaultBrowserEventSource';
 
-// Temporary exports for testing the events source in a browser.
-export { DefaultBrowserEventSource };
+// Temporary exports for testing in a browser.
+export { DefaultBrowserEventSource, BrowserInfo };
 export * from '@launchdarkly/js-client-sdk-common';
 
 export function Hello() {
