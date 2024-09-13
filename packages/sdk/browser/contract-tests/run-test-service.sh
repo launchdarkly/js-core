3f02658ed14e94bd61e8a8ea8cb807dc59037349
Ryan Lamb <4955475+kinyoklion@users.noreply.github.com>
2024-09-13 13:36:48 -0700

    Package script adjustments.

--- packages/sdk/browser/contract-tests/run-test-service.sh
+++ packages/sdk/browser/contract-tests/run-test-service.sh
@@ -1,1 +1,1 @@
-yarn workspace browser-contract-test-adapter run start & yarn workspace browser-contract-test-service run dev && kill $!
+yarn workspace browser-contract-test-adapter run start & yarn workspace browser-contract-test-service run start && kill $!
