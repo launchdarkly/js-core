2f730ab446f099e3fab562f19d5abfc2e7ffb7a1
Ryan Lamb <4955475+kinyoklion@users.noreply.github.com>
2024-09-19 10:57:49 -0700

    Revert jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   transform: { '^.+\\.ts?$': 'ts-jest' },
   testMatch: ['**/__tests__/**/*test.ts?(x)'],
   testEnvironment: 'node',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node', 'json'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   collectCoverageFrom: [
     'packages/sdk/server-node/src/**/*.ts',
     'packages/shared/common/src/**/*.ts',
