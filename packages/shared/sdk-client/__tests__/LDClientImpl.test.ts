20af513c7b4e55fdd596a8f84a3151dbf136dc35
Ryan Lamb <4955475+kinyoklion@users.noreply.github.com>
2024-09-25 14:37:59 -0700

    Fix client-side tests.

--- packages/shared/sdk-client/__tests__/LDClientImpl.test.ts
+++ packages/shared/sdk-client/__tests__/LDClientImpl.test.ts
@@ -86,6 +86,8 @@ describe('sdk-client object', () => {
       'easter-i-tunes-special': false,
       'easter-specials': 'no specials',
       fdsafdsafdsafdsa: true,
+      'has-prereq-depth-1': true,
+      'is-prereq': true,
       'log-level': 'warn',
       'moonshot-demo': true,
       test1: 's1',
