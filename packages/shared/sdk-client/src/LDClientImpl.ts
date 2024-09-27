eef7aff62659367a77d506ef8d6041e93f1a95ee
Ryan Lamb <4955475+kinyoklion@users.noreply.github.com>
2024-09-27 08:47:54 -0700

    chore: Remove unused function in common client-side LDClientImpl.

--- packages/shared/sdk-client/src/LDClientImpl.ts
+++ packages/shared/sdk-client/src/LDClientImpl.ts
@@ -170,64 +170,6 @@ export default class LDClientImpl implements LDClient {
     return this.checkedContext;
   }
 
-  private createStreamListeners(
-    context: Context,
-    identifyResolve: any,
-  ): Map<StreamEventName, ProcessStreamResponse> {
-    const listeners = new Map<StreamEventName, ProcessStreamResponse>();
-
-    listeners.set('put', {
-      deserializeData: JSON.parse,
-      processJson: async (evalResults: Flags) => {
-        this.logger.debug(`Stream PUT: ${Object.keys(evalResults)}`);
-
-        // mapping flags to item descriptors
-        const descriptors = Object.entries(evalResults).reduce(
-          (acc: { [k: string]: ItemDescriptor }, [key, flag]) => {
-            acc[key] = { version: flag.version, flag };
-            return acc;
-          },
-          {},
-        );
-        await this.flagManager.init(context, descriptors).then(identifyResolve());
-      },
-    });
-
-    listeners.set('patch', {
-      deserializeData: JSON.parse,
-      processJson: async (patchFlag: PatchFlag) => {
-        this.logger.debug(`Stream PATCH ${JSON.stringify(patchFlag, null, 2)}`);
-        this.flagManager.upsert(context, patchFlag.key, {
-          version: patchFlag.version,
-          flag: patchFlag,
-        });
-      },
-    });
-
-    listeners.set('delete', {
-      deserializeData: JSON.parse,
-      processJson: async (deleteFlag: DeleteFlag) => {
-        this.logger.debug(`Stream DELETE ${JSON.stringify(deleteFlag, null, 2)}`);
-
-        this.flagManager.upsert(context, deleteFlag.key, {
-          version: deleteFlag.version,
-          flag: {
-            ...deleteFlag,
-            deleted: true,
-            // props below are set to sensible defaults. they are irrelevant
-            // because this flag has been deleted.
-            flagVersion: 0,
-            value: undefined,
-            variation: 0,
-            trackEvents: false,
-          },
-        });
-      },
-    });
-
-    return listeners;
-  }
-
   private createIdentifyPromise(timeout: number): {
     identifyPromise: Promise<void>;
     identifyResolve: () => void;
