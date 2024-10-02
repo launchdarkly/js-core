676477ac9559174e7a6a13fb6c66ccd9749393f8
Ryan Lamb <4955475+kinyoklion@users.noreply.github.com>
2024-10-02 10:35:13 -0700

    Remove log handler.

--- packages/shared/common/rollup.config.js
+++ packages/shared/common/rollup.config.js
@@ -21,11 +21,6 @@ const getSharedConfig = (format, file) => ({
       file: file,
     },
   ],
-  onwarn: (warning) => {
-    if (warning.code !== 'CIRCULAR_DEPENDENCY') {
-      console.error(`(!) ${warning.message}`);
-    }
-  },
 });
 
 export default [
