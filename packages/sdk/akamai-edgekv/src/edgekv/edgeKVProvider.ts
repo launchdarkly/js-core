6f95c1f02b5ec75c09147ecaca003ac0cc46d30d
Clifford Tawiah <ctawiah@launchdarkly.com>
2024-10-15 11:10:51 -0400

    fix: added logging when retrieving values from with edgkv lib fails

--- packages/sdk/akamai-edgekv/src/edgekv/edgeKVProvider.ts
+++ packages/sdk/akamai-edgekv/src/edgekv/edgeKVProvider.ts
@@ -1,24 +1,27 @@
-import { EdgeProvider } from '@launchdarkly/akamai-edgeworker-sdk-common';
+import { BasicLogger, EdgeProvider } from '@launchdarkly/js-server-sdk-common';
 
 import { EdgeKV } from './edgekv';
 
 type EdgeKVProviderParams = {
   namespace: string;
   group: string;
+  logger: BasicLogger;
 };
 
 export default class EdgeKVProvider implements EdgeProvider {
   private edgeKv: EdgeKV;
+  private logger: BasicLogger;
 
-  constructor({ namespace, group }: EdgeKVProviderParams) {
+  constructor({ namespace, group, logger }: EdgeKVProviderParams) {
     this.edgeKv = new EdgeKV({ namespace, group } as any);
+    this.logger = logger;
   }
 
   async get(rootKey: string): Promise<string | null | undefined> {
     try {
       return await this.edgeKv.getText({ item: rootKey } as any);
     } catch (e) {
-      /* empty */
+      this.logger?.error(`Error getting value from EdgeKV: ${e}`);
     }
     return undefined;
   }
