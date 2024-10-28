979219bce6655126a4b6341f5dba92c970e46e94
Ryan Lamb <4955475+kinyoklion@users.noreply.github.com>
2024-10-28 08:58:46 -0700

    feat: Refine CJS/ESM build configuration for browser SDK.

---
+++ packages/sdk/browser/tsup.config.ts
@@ -0,0 +1,25 @@
+// It is a dev dependency and the linter doesn't understand.
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  minify: 'terser',
+  terserOptions: {
+    mangle: {
+      properties: {
+        // Mangle class properties which start with an underscore.
+        regex: /^_/,
+        // Do not mangle '_meta', because this is part of our JSON
+        // data model.
+        reserved: ['_meta'],
+      },
+    },
+  },
+  format: ['esm', 'cjs'],
+  entry: ['src/index.ts'],
+  splitting: false,
+  sourcemap: false,
+  clean: true,
+  dts: true,
+  treeshake: true,
+});
