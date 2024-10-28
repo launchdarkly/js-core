87ce8e0e5a53e6f76ab84389017312c7e3385e7e
Ryan Lamb <4955475+kinyoklion@users.noreply.github.com>
2024-10-28 14:59:59 -0700

    Use esbuild options which removes dependence on rollup and terser.

--- packages/sdk/browser/tsup.config.ts
+++ packages/sdk/browser/tsup.config.ts
@@ -7,23 +7,21 @@ export default defineConfig({
     index: 'src/index.ts',
     compat: 'src/compat/index.ts'
   },
-  minify: 'terser',
-  terserOptions: {
-    mangle: {
-      properties: {
-        // Mangle class properties which start with an underscore.
-        regex: /^_/,
-        // Do not mangle '_meta', because this is part of our JSON
-        // data model.
-        reserved: ['_meta'],
-      },
-    },
-  },
+  minify: true,
   format: ['esm', 'cjs'],
   splitting: false,
   sourcemap: false,
   clean: true,
   noExternal: ['@launchdarkly/js-sdk-common', '@launchdarkly/js-client-sdk-common'],
-  treeshake: true,
   dts: true,
+  metafile: true,
+  minifyIdentifiers: true,
+  esbuildOptions(opts) {
+    // This would normally be `^_(?!meta|_)`, but go doesn't support negative look-ahead assertions,
+    // so we need to craft something that works without it.
+    // So start of line followed by a character that isn't followed by m or underscore, but we
+    // want other things that do start with m, so we need to progressively handle more characters
+    // of meta with exclusions.
+    opts.mangleProps = /^_([^m|_]|m[^e]|me[^t]|met[^a])/;
+  },
 });
