7d91ea88f37b6ff7944befd38a86ac4d0779c53a
Ryan Lamb <4955475+kinyoklion@users.noreply.github.com>
2024-11-04 13:17:37 -0800

    Commenting improvements.

--- packages/sdk/ai/src/index.ts
+++ packages/sdk/ai/src/index.ts
@@ -19,20 +19,28 @@ export interface AIClient {
   interpolateTemplate(template: string, variables: Record<string, unknown>): string;
 
   /**
-   * Retrieves and processes a prompt template based on the provided key, LaunchDarkly context, and variables.
+   * Retrieves and processes a prompt template based on the provided key, LaunchDarkly context, and
+   * variables.
    *
-   * @param key - A unique identifier for the prompt template. This key is used to fetch the correct prompt from storage or configuration.
-   * @param context - The LaunchDarkly context object that contains relevant information about the current environment, user, or session. This context may influence how the prompt is processed or personalized.
-   * @param variables - A map of key-value pairs representing dynamic variables to be injected into the prompt template. The keys correspond to placeholders within the template, and the values are the corresponding replacements.
-   * @param defaultValue - A fallback value to be used if the prompt template associated with the key is not found or if any errors occur during processing.
+   * @param key - A unique identifier for the prompt template. This key is used to fetch the correct
+   *  prompt from storage or configuration.
+   * @param context - The LaunchDarkly context object that contains relevant information about the
+   * current environment, user, or session. This context may influence how the prompt is processed
+   * or personalized.
+   * @param variables - A map of key-value pairs representing dynamic variables to be injected into
+   * the prompt template. The keys correspond to placeholders within the template, and the values
+   * are the corresponding replacements.
+   * @param defaultValue - A fallback value to be used if the prompt template associated with the
+   * key is not found or if any errors occur during processing.
    *
-   * @returns The processed prompt after all variables have been substituted in the stored prompt template. If the prompt cannot be retrieved or processed, the `defaultValue` is returned.
+   * @returns The processed prompt after all variables have been substituted in the stored prompt
+   * template. If the prompt cannot be retrieved or processed, the `defaultValue` is returned.
    *
    * @example
    * ```
    * const key = "welcome_prompt";
    * const context = {...};
-   * const variables = {username: 'john};
+   * const variables = {username: 'john'};
    * const defaultValue = {};
    *
    * const result = modelConfig(key, context, defaultValue, variables);
