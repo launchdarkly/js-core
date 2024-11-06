6271e9631e87a183e25737ce0cd9595a9f5937dc
Ryan Lamb <4955475+kinyoklion@users.noreply.github.com>
2024-11-06 14:07:27 -0800

    fix: Remove underscore token usage. Improve documentation.

--- packages/sdk/server-ai/src/api/LDAIClient.ts
+++ packages/sdk/server-ai/src/api/LDAIClient.ts
@@ -17,37 +17,43 @@ export interface LDAIClient {
   interpolateTemplate(template: string, variables: Record<string, unknown>): string;
 
   /**
-   * Retrieves and processes a prompt template based on the provided key, LaunchDarkly context, and
-   * variables.
+   * Retrieves and processes an AI configuration based on the provided key, LaunchDarkly context,
+   * and variables. This includes the model configuration and the processed prompts.
    *
-   * @param key - A unique identifier for the prompt template. This key is used to fetch the correct
-   *  prompt from storage or configuration.
-   * @param context - The LaunchDarkly context object that contains relevant information about the
-   * current environment, user, or session. This context may influence how the prompt is processed
-   * or personalized.
-   * @param variables - A map of key-value pairs representing dynamic variables to be injected into
+   * @param key The key of the AI configuration.
+   * @param context The LaunchDarkly context object that contains relevant information about the
+   * current environment, user, or session. This context may influence how the configuration is
+   * processed or personalized.
+   * @param variables A map of key-value pairs representing dynamic variables to be injected into
    * the prompt template. The keys correspond to placeholders within the template, and the values
    * are the corresponding replacements.
-   * @param defaultValue - A fallback value to be used if the prompt template associated with the
-   * key is not found or if any errors occur during processing.
+   * @param defaultValue A fallback value containing model configuration and prompts. This will
+   * be used if the configurationuration is not available from launchdarkly.
    *
-   * @returns The processed prompt after all variables have been substituted in the stored prompt
-   * template. If the prompt cannot be retrieved or processed, the `defaultValue` is returned.
+   * @returns The AI configurationuration including a processed prompt after all variables have been
+   * substituted in the stored prompt template. This will also include a `tracker` used to track
+   * the state of the AI operation. If the configuration cannot be accessed from LaunchDarkly, then
+   * the return value will include information from the defaultValue.
    *
    * @example
    * ```
    * const key = "welcome_prompt";
    * const context = {...};
    * const variables = {username: 'john'};
-   * const defaultValue = {};
+   * const defaultValue = {
+   *  enabled: false,
+   * };
    *
    * const result = modelConfig(key, context, defaultValue, variables);
    * // Output:
    * {
-   *   modelId: "gpt-4o",
-   *   temperature: 0.2,
-   *   maxTokens: 4096,
-   *   userDefinedKey: "myValue",
+   *   enabled: true,
+   *   config: {
+   *     modelId: "gpt-4o",
+   *     temperature: 0.2,
+   *     maxTokens: 4096,
+   *     userDefinedKey: "myValue",
+   *   },
    *   prompt: [
    *     {
    *       role: "system",
@@ -57,7 +63,8 @@ export interface LDAIClient {
    *       role: "user",
    *       content: "Explain how you're an amazing GPT."
    *     }
-   *   ]
+   *   ],
+   *   tracker: ...
    * }
    * ```
    */
--- packages/sdk/server-ai/src/api/metrics/UnderScoreTokenUsage.ts
+++
@@ -1,9 +0,0 @@
-import { LDTokenUsage } from './LDTokenUsage';
-
-export function createUnderscoreTokenUsage(data: any): LDTokenUsage {
-  return {
-    total: data.total_tokens || 0,
-    input: data.prompt_tokens || 0,
-    output: data.completion_tokens || 0,
-  };
-}
