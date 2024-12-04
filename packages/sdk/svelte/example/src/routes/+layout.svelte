1572639b7581b3b7d9b2693cd61000eb3025ae73
Robinson Marquez <robinsonmadrid@outlook.com>
2024-12-04 19:25:42 -0300

    feat: add Svelte example project with configuration and initial tests

---
+++ packages/sdk/svelte/example/src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { LDProvider } from '@launchdarkly/svelte-client-sdk';
+	import { PUBLIC_LD_CLIENT_ID } from '$env/static/public';
+
+	const context = {
+		kind: 'user',
+		key: 'example-context-key',
+		name: 'Sandy'
+	};
+</script>
+
+<div>
+	<LDProvider clientID={PUBLIC_LD_CLIENT_ID} {context}>
+		<slot />
+
+		<p slot="initializing">loading flags...</p>
+	</LDProvider>
+</div>
