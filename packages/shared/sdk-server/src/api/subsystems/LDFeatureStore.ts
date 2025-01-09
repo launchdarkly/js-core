f54db93af88bb2a7fad026f1e76ca3c0244d4ac3
Todd Anderson <tanderson@launchdarkly.com>
2025-01-09 14:57:25 -0600

    adds comments

--- packages/shared/sdk-server/src/api/subsystems/LDFeatureStore.ts
+++ packages/shared/sdk-server/src/api/subsystems/LDFeatureStore.ts
@@ -137,6 +137,16 @@ export interface LDFeatureStore {
    */
   upsert(kind: DataKind, data: LDKeyedFeatureStoreItem, callback: () => void): void;
 
+  /**
+   * @param basis If true, completely overwrites the current contents of the data store
+   * with the provided data.  If false, upserts the items in the provided data.  Upserts
+   * are made only if provided items have newer versions than existing items.
+   * @param data An object in which each key is the "namespace" of a collection (e.g. `"features"`) and
+   * the value is an object that maps keys to entities. The actual type of this parameter is
+   * `interfaces.FullDataSet<VersionedData>`.
+   * @param selector TODO
+   * @param callback Will be called after the changes are applied.
+   */
   applyChanges(
     basis: boolean,
     data: LDFeatureStoreDataStorage,
