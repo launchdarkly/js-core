68a3b87fcc9600a7f64e7e2e1a15c12b9c370f25
Ryan Lamb <4955475+kinyoklion@users.noreply.github.com>
2025-01-21 09:18:02 -0800

    feat: Add singleton support for browser-telemetry. (#739)
    This PR adds a singleton interface to the EM SDK to simplify common
    usage.
    
    You can still create individual browser telemetry instances, but there
    is a default instance with module level functions for each operation.
    
    Minimal example.
    ```
    import * as LDTelemetry from '@launchdarkly/browser-telemetry`
    
    LDTelemetry.initTelemetry();
    
    LDTelemetry.register(ldClient);
    
    // For manual error capture.
    LDTelemetry.captureError(someError);
    ```
    
    ---------
    
    Co-authored-by: Stacy Harrison <11901347+stasquatch@users.noreply.github.com>

--- packages/telemetry/browser-telemetry/src/index.ts
+++ packages/telemetry/browser-telemetry/src/index.ts
@@ -6,7 +6,20 @@ import parse from './options';
 
 export * from './api';
 
-export function initializeTelemetry(options?: Options): BrowserTelemetry {
+export * from './singleton';
+
+/**
+ * Initialize a new telemetry instance.
+ *
+ * This instance is not global. Generally developers should use {@link initializeTelemetry} instead.
+ *
+ * If for some reason multiple telemetry instances are needed, this method can be used to create a new instance.
+ * Instances are not aware of each other and may send duplicate data from automatically captured events.
+ *
+ * @param options The options to use for the telemetry instance.
+ * @returns A telemetry instance.
+ */
+export function initializeTelemetryInstance(options?: Options): BrowserTelemetry {
   const parsedOptions = parse(options || {}, safeMinLogger(options?.logger));
   return new BrowserTelemetryImpl(parsedOptions);
 }
---
+++ packages/telemetry/browser-telemetry/src/singleton/singletonInstance.ts
@@ -0,0 +1,100 @@
+import { Options } from '../api';
+import { BrowserTelemetry } from '../api/BrowserTelemetry';
+import BrowserTelemetryImpl from '../BrowserTelemetryImpl';
+import { fallbackLogger, prefixLog, safeMinLogger } from '../logging';
+import parse from '../options';
+
+let telemetryInstance: BrowserTelemetry | undefined;
+let warnedClientNotInitialized: boolean = false;
+
+/**
+ * Initialize the LaunchDarkly telemetry client
+ *
+ * This method should be called one time as early as possible in the application lifecycle.
+ *
+ * @example
+ * ```
+ * import { initTelemetry } from '@launchdarkly/browser-telemetry';
+ *
+ * initTelemetry();
+ * ```
+ *
+ * After initialization the telemetry client must be registered with the LaunchDarkly SDK client.
+ *
+ * @example
+ * ```
+ * import { initTelemetry, register } from '@launchdarkly/browser-telemetry';
+ *
+ * initTelemetry();
+ *
+ * // Create your LaunchDarkly client following the LaunchDarkly SDK documentation.
+ *
+ * register(ldClient);
+ * ```
+ *
+ * If using the 3.x version of the LaunchDarkly SDK, then you must also add inspectors when initializing your LaunchDarkly client.
+ * This allows for integration with feature flag data.
+ *
+ * @example
+ * ```
+ * import { initTelemetry, register, inspectors } from '@launchdarkly/browser-telemetry';
+ * import { init } from 'launchdarkly-js-client-sdk';
+ *
+ * initTelemetry();
+ *
+ * const ldClient = init('YOUR_CLIENT_SIDE_ID', {
+ *   inspectors: inspectors()
+ * });
+ *
+ * register(ldClient);
+ * ```
+ *
+ * @param options The options to use for the telemetry instance. Refer to {@link Options} for more information.
+ */
+export function initTelemetry(options?: Options) {
+  const logger = safeMinLogger(options?.logger);
+
+  if (telemetryInstance) {
+    logger.warn(prefixLog('Telemetry has already been initialized. Ignoring new options.'));
+    return;
+  }
+
+  const parsedOptions = parse(options || {}, logger);
+  telemetryInstance = new BrowserTelemetryImpl(parsedOptions);
+}
+
+/**
+ * Get the telemetry instance.
+ *
+ * In typical operation this method doesn't need to be called. Instead the functions exported by this package directly
+ * use the telemetry instance.
+ *
+ * This function can be used when the telemetry instance needs to be injected into code instead of accessed globally.
+ *
+ * @returns The telemetry instance, or undefined if it has not been initialized.
+ */
+export function getTelemetryInstance(): BrowserTelemetry | undefined {
+  if (!telemetryInstance) {
+    if (warnedClientNotInitialized) {
+      return undefined;
+    }
+
+    fallbackLogger.warn(prefixLog('Telemetry has not been initialized'));
+    warnedClientNotInitialized = true;
+    return undefined;
+  }
+
+  return telemetryInstance;
+}
+
+/**
+ * Reset the telemetry instance to its initial state.
+ *
+ * This method is intended to be used in tests.
+ *
+ * @internal
+ */
+export function resetTelemetryInstance() {
+  telemetryInstance = undefined;
+  warnedClientNotInitialized = false;
+}
