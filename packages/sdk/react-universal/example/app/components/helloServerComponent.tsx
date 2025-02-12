6ec07d156f96636b5246c6f9453b7f6bf13f78e8
Michael Anderjaska <m.anderjaska@gmail.com>
2025-02-12 12:42:33 -0500

    update example

--- packages/sdk/react-universal/example/app/components/helloServerComponent.tsx
+++ packages/sdk/react-universal/example/app/components/helloServerComponent.tsx
@@ -1,9 +1,9 @@
 import { getLDContext } from '@/app/utils';
 
-import { useVariationRsc } from '@launchdarkly/react-universal-sdk/server';
+import { useBoolVariationDetailRsc } from '@launchdarkly/react-universal-sdk/server';
 
 export default async function HelloServerComponent() {
-  const flagValue = await useVariationRsc('my-boolean-flag-1', getLDContext());
+  const flagValue = await useBoolVariationDetailRsc('my-boolean-flag-1', getLDContext(), false);
 
   return (
     <div className="border-2 border-white/20 p-4">
