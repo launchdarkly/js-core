24024387ae18d90d392da93f31a49df5944289ae
Michael Anderjaska <m.anderjaska@gmail.com>
2025-02-12 12:30:11 -0500

    chore: update LD provider

--- packages/sdk/react-universal/src/types.ts
+++ packages/sdk/react-universal/src/types.ts
@@ -1,6 +1,6 @@
 import type { LDClient as NodeSdk } from '@launchdarkly/node-server-sdk';
 
-export type { LDClient as JSSdk } from 'launchdarkly-js-client-sdk';
+export type { LDClient as JSSdk } from '@launchdarkly/js-client-sdk';
 
 export type { NodeSdk };
 
