ac0d3c1be73ae35f38492da7b81ade1eb138c710
Yusinto Ngadiman <yusinto@gmail.com>
2025-02-12 12:21:13 -0800

    fix: Fixed bug where variationDetail value is treated as boolean.

--- packages/sdk/react-universal/example/app/components/helloServerComponent.tsx
+++ packages/sdk/react-universal/example/app/components/helloServerComponent.tsx
@@ -3,12 +3,12 @@ import { getLDContext } from '@/app/utils';
 import { useBoolVariationDetailRsc } from '@launchdarkly/react-universal-sdk/server';
 
 export default async function HelloServerComponent() {
-  const flagValue = await useBoolVariationDetailRsc('my-boolean-flag-1', getLDContext(), false);
+  const result = await useBoolVariationDetailRsc('my-boolean-flag-1', getLDContext(), false);
 
   return (
     <div className="border-2 border-white/20 p-4">
       <p className="text-xl ldgradient">
-        {flagValue
+        {result.value
           ? 'This flag is evaluating True in a React Server Component'
           : 'This flag is evaluating False in a React Server Component'}
       </p>
