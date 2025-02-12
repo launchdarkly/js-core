39ad5d14d22844cc958f4727576fef54f1e3ec74
Michael Anderjaska <m.anderjaska@gmail.com>
2025-02-12 12:40:54 -0500

    feat: add more hooks to sdk

--- packages/sdk/react-universal/src/client/hooks/variation/useTypedVariation.ts
+++ packages/sdk/react-universal/src/client/hooks/variation/useTypedVariation.ts
@@ -9,15 +9,15 @@ export const useTypedVariation = <T extends boolean | number | string | unknown>
   const ldClient = useLDClient();
 
   switch (typeof defaultValue) {
-    // case 'boolean':
-    //   return ldClient.boolVariation(key, defaultValue as boolean) as T;
-    // case 'number':
-    //   return ldClient.numberVariation(key, defaultValue as number) as T;
-    // case 'string':
-    //   return ldClient.stringVariation(key, defaultValue as string) as T;
-    // case 'undefined':
-    // case 'object':
-    //   return ldClient.jsonVariation(key, defaultValue) as T;
+    case 'boolean':
+      return ldClient.boolVariation(key, defaultValue as boolean) as T;
+    case 'number':
+      return ldClient.numberVariation(key, defaultValue as number) as T;
+    case 'string':
+      return ldClient.stringVariation(key, defaultValue as string) as T;
+    case 'undefined':
+    case 'object':
+      return ldClient.jsonVariation(key, defaultValue) as T;
     default:
       return ldClient.variation(key, defaultValue);
   }
@@ -30,24 +30,24 @@ export const useTypedVariationDetail = <T extends boolean | number | string | un
   const ldClient = useLDClient();
 
   switch (typeof defaultValue) {
-    // case 'boolean':
-    //   return ldClient.boolVariationDetail(
-    //     key,
-    //     defaultValue as boolean,
-    //   ) as LDEvaluationDetailTyped<T>;
-    // case 'number':
-    //   return ldClient.numberVariationDetail(
-    //     key,
-    //     defaultValue as number,
-    //   ) as LDEvaluationDetailTyped<T>;
-    // case 'string':
-    //   return ldClient.stringVariationDetail(
-    //     key,
-    //     defaultValue as string,
-    //   ) as LDEvaluationDetailTyped<T>;
-    // case 'undefined':
-    // case 'object':
-    //   return ldClient.jsonVariationDetail(key, defaultValue) as LDEvaluationDetailTyped<T>;
+    case 'boolean':
+      return ldClient.boolVariationDetail(
+        key,
+        defaultValue as boolean,
+      ) as LDEvaluationDetailTyped<T>;
+    case 'number':
+      return ldClient.numberVariationDetail(
+        key,
+        defaultValue as number,
+      ) as LDEvaluationDetailTyped<T>;
+    case 'string':
+      return ldClient.stringVariationDetail(
+        key,
+        defaultValue as string,
+      ) as LDEvaluationDetailTyped<T>;
+    case 'undefined':
+    case 'object':
+      return ldClient.jsonVariationDetail(key, defaultValue) as LDEvaluationDetailTyped<T>;
     default:
       return ldClient.variationDetail(key, defaultValue) as LDEvaluationDetailTyped<T>;
   }
--- packages/sdk/react-universal/src/server/hooks/variation/useVariationRsc.ts
+++ packages/sdk/react-universal/src/server/hooks/variation/useVariationRsc.ts
@@ -2,9 +2,53 @@ import type { LDContext, LDFlagValue } from '@launchdarkly/node-server-sdk';
 
 import { useLDClientRsc } from '../useLDClientRsc';
 
+export const useBoolVariationRsc = async (key: string, context: LDContext, def: boolean) => {
+  const ldc = await useLDClientRsc(context);
+  return ldc.boolVariation(key, def);
+};
+
+export const useStringVariationRsc = async (key: string, context: LDContext, def: string) => {
+  const ldc = await useLDClientRsc(context);
+  return ldc.stringVariation(key, def);
+};
+
+export const useNumberVariationRsc = async (key: string, context: LDContext, def: number) => {
+  const ldc = await useLDClientRsc(context);
+  return ldc.numberVariation(key, def);
+};
+
+export const useJsonVariationRsc = async (key: string, context: LDContext, def: undefined) => {
+  const ldc = await useLDClientRsc(context);
+  return ldc.jsonVariation(key, def);
+};
+
 export const useVariationRsc = async (key: string, context: LDContext, def?: LDFlagValue) => {
   const ldc = await useLDClientRsc(context);
   return ldc.variation(key, def);
+};
+
+export const useBoolVariationDetailRsc = async (key: string, context: LDContext, def: boolean) => {
+  const ldc = await useLDClientRsc(context);
+  return ldc.boolVariationDetail(key, def);
+};
+
+export const useStringVariationDetailRsc = async (key: string, context: LDContext, def: string) => {
+  const ldc = await useLDClientRsc(context);
+  return ldc.stringVariationDetail(key, def);
+};
+
+export const useNumberVariationDetailRsc = async (key: string, context: LDContext, def: number) => {
+  const ldc = await useLDClientRsc(context);
+  return ldc.numberVariationDetail(key, def);
+};
+
+export const useJsonVariationDetailRsc = async (
+  key: string,
+  context: LDContext,
+  def: undefined,
+) => {
+  const ldc = await useLDClientRsc(context);
+  return ldc.jsonVariationDetail(key, def);
 };
 
 export const useVariationDetailRsc = async (key: string, context: LDContext, def?: LDFlagValue) => {
