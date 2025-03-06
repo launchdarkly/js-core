bf72203379e59d676d838a5e0b3764fdb06535d7
Henry Barrow <hbarrow@launchdarkly.com>
2025-03-06 17:31:06 +0000

    Add lazy loader

--- packages/sdk/fastly/src/api/EdgeFeatureStore.ts
+++ packages/sdk/fastly/src/api/EdgeFeatureStore.ts
@@ -14,6 +14,7 @@ export interface EdgeProvider {
 
 export class EdgeFeatureStore implements LDFeatureStore {
   private readonly _rootKey: string;
+  private _kvData: string | null | undefined;
 
   constructor(
     private readonly _edgeProvider: EdgeProvider,
@@ -22,6 +23,18 @@ export class EdgeFeatureStore implements LDFeatureStore {
     private _logger: LDLogger,
   ) {
     this._rootKey = `LD-Env-${sdkKey}`;
+  }
+
+  /**
+   * This function is used to lazy load the KV data from the edge provider. This is necessary because Fastly Compute
+   * has a limit of 32 backend requests (including requests to fetch the KV data).
+   * https://docs.fastly.com/products/compute-resource-limits
+   */
+  private async _getKVData(): Promise<string | null | undefined> {
+    if (!this._kvData) {
+      this._kvData = await this._edgeProvider.get(this._rootKey);
+    }
+    return this._kvData;
   }
 
   async get(
@@ -34,7 +47,7 @@ export class EdgeFeatureStore implements LDFeatureStore {
     this._logger.debug(`Requesting ${dataKey} from ${this._rootKey}.${kindKey}`);
 
     try {
-      const i = await this._edgeProvider.get(this._rootKey);
+      const i = await this._getKVData();
 
       if (!i) {
         throw new Error(`${this._rootKey}.${kindKey} is not found in KV.`);
@@ -66,7 +79,7 @@ export class EdgeFeatureStore implements LDFeatureStore {
     const kindKey = namespace === 'features' ? 'flags' : namespace;
     this._logger.debug(`Requesting all from ${this._rootKey}.${kindKey}`);
     try {
-      const i = await this._edgeProvider.get(this._rootKey);
+      const i = await this._getKVData();
       if (!i) {
         throw new Error(`${this._rootKey}.${kindKey} is not found in KV.`);
       }
@@ -93,7 +106,7 @@ export class EdgeFeatureStore implements LDFeatureStore {
   }
 
   async initialized(callback: (isInitialized: boolean) => void = noop): Promise<void> {
-    const config = await this._edgeProvider.get(this._rootKey);
+    const config = await this._getKVData();
     const result = config !== null;
     this._logger.debug(`Is ${this._rootKey} initialized? ${result}`);
     callback(result);
