18110223878dbf972bdc682164301d0907880bcb
Matthew Keeler <mkeeler@launchdarkly.com>
2025-03-17 15:54:53 -0400

    Add doc

--- packages/shared/sdk-server-edge/src/api/cache.ts
+++ packages/shared/sdk-server-edge/src/api/cache.ts
@@ -1,5 +1,24 @@
+/**
+ * General-purpose cache interface.
+ *
+ * This is used by the SDK to cache feature flags and other data. The SDK does
+ * not assume any particular implementation of the cache, so you can provide
+ * your own.
+ */
 export default interface Cache {
+  /**
+  * Get a value from the cache. Returning `undefined` means the key was not found.
+  */
   get(key: string): any;
+
+  /**
+  * Set a value in the cache.
+  */
   set(key: string, value: any): void;
+
+  /**
+  * The close method offers a way to clean up any resources used by the cache
+  * on shutdown.
+  */
   close(): void;
 }
