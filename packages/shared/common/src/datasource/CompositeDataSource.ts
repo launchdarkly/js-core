bd1ac1dbe5596885bade65cc86963cc1a5d2b65a
Todd Anderson <tanderson@launchdarkly.com>
2025-03-28 10:44:26 -0500

    fixing delayed transitions not cleaning up their own cancel tokens

--- packages/shared/common/src/datasource/CompositeDataSource.ts
+++ packages/shared/common/src/datasource/CompositeDataSource.ts
@@ -135,10 +135,12 @@ export class CompositeDataSource implements DataSource {
                     const { promise, cancel } = this._cancellableDelay(condition.durationMS);
                     cancelScheduledTransition = cancel;
                     this._cancelTokens.push(cancelScheduledTransition);
-                    promise.then(() => {
-                      callbackHandler.disable();
-                      transitionResolve({ transition: condition.transition });
-                    });
+                    promise
+                      .then(() => this._consumeCancelToken(cancel))
+                      .then(() => {
+                        callbackHandler.disable();
+                        transitionResolve({ transition: condition.transition });
+                      });
                   } else {
                     // this data source state does not have a transition condition, so don't schedule any transition
                   }
@@ -176,7 +178,9 @@ export class CompositeDataSource implements DataSource {
         const delay = this._backoff.fail();
         const { promise, cancel: cancelDelay } = this._cancellableDelay(delay);
         this._cancelTokens.push(cancelDelay);
-        const delayedTransition = promise.then(() => transitionRequest);
+        const delayedTransition = promise
+          .then(() => this._consumeCancelToken(cancelDelay))
+          .then(() => transitionRequest);
 
         // race the delayed transition and external transition requests to be responsive
         transitionRequest = await Promise.race([
