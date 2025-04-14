2bc5b493ec0a49ec3b5bb173617b6978d7f42d66
Ryan Lamb <4955475+kinyoklion@users.noreply.github.com>
2025-04-14 14:34:40 -0700

    Some cleanup.

--- packages/sdk/browser/src/platform/BrowserRequests.ts
+++ packages/sdk/browser/src/platform/BrowserRequests.ts
@@ -11,7 +11,6 @@ import DefaultBrowserEventSource from './DefaultBrowserEventSource';
 
 export default class BrowserRequests implements Requests {
   fetch(url: string, options?: Options): Promise<Response> {
-    delete options?.compressBodyIfPossible;
     // @ts-ignore
     return fetch(url, options);
   }
