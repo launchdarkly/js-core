a3b816b12d3386d50e206f31e1dc24a1078ebbc8
Todd Anderson <127344469+tanderson-ld@users.noreply.github.com>
2025-04-25 11:24:06 -0500

    chore: adds DataSource interface, CompositeDataSource, and FDv2 paylo… (#830)
    **Requirements**
    
    - [x] I have added test coverage for new or changed functionality
    - [x] I have followed the repository's [pull request submission
    guidelines](../blob/main/CONTRIBUTING.md#submitting-pull-requests)
    - [ ] I have validated my changes against all supported platform
    versions
    Classes are independent of platforms for the moment, and also not
    reachable. (Except for Backoff which was moved).
    
    This PR moves shared/common changes for FDv2 from the temporary holding
    branch to main. All code has been previously reviewed and was picked
    from the temporary holding branch using file pick commands.
    
    Additional prettier fix commits.
    
    ---------
    
    Co-authored-by: Todd Anderson <tanderson@Todds-MacBook-Pro.local>

--- packages/shared/common/src/api/subsystem/index.ts
+++ packages/shared/common/src/api/subsystem/index.ts
@@ -1,9 +1,13 @@
+import { DataSource, DataSourceState, LDDataSourceFactory } from './DataSystem';
 import LDContextDeduplicator from './LDContextDeduplicator';
 import LDEventProcessor from './LDEventProcessor';
 import LDEventSender, { LDDeliveryStatus, LDEventSenderResult, LDEventType } from './LDEventSender';
 import { LDStreamProcessor } from './LDStreamProcessor';
 
 export {
+  DataSource,
+  DataSourceState,
+  LDDataSourceFactory,
   LDEventProcessor,
   LDContextDeduplicator,
   LDEventSender,
---
+++ packages/shared/common/src/internal/fdv2/payloadStreamReader.ts
@@ -0,0 +1,71 @@
+/* eslint-disable no-underscore-dangle */
+import { EventListener, EventName, LDLogger } from '../../api';
+import { DataSourceErrorKind } from '../../datasource';
+import { ObjProcessors, PayloadListener, PayloadProcessor } from './payloadProcessor';
+
+/**
+ * Interface for an event stream.  Only allows listening to events.
+ */
+export interface EventStream {
+  addEventListener(type: EventName, listener: EventListener): void;
+}
+
+/**
+ * A FDv2 PayloadStreamReader can be used to parse payloads from a stream of FDv2 events.  See {@link PayloadProcessor}
+ * for more details.
+ */
+export class PayloadStreamReader {
+  private _payloadProcessor: PayloadProcessor;
+
+  /**
+   * Creates a PayloadStreamReader
+   *
+   * @param eventStream event stream of FDv2 events
+   * @param _objProcessors defines object processors for each object kind.
+   * @param _errorHandler that will be called with parsing errors as they are encountered
+   * @param _logger for logging
+   */
+  constructor(
+    eventStream: EventStream,
+    _objProcessors: ObjProcessors,
+    private readonly _errorHandler?: (errorKind: DataSourceErrorKind, message: string) => void,
+    private readonly _logger?: LDLogger,
+  ) {
+    this._attachHandler(eventStream, 'server-intent');
+    this._attachHandler(eventStream, 'put-object');
+    this._attachHandler(eventStream, 'delete-object');
+    this._attachHandler(eventStream, 'payload-transferred');
+    this._attachHandler(eventStream, 'goodbye');
+    this._attachHandler(eventStream, 'error');
+    this._payloadProcessor = new PayloadProcessor(_objProcessors, _errorHandler, _logger);
+  }
+
+  addPayloadListener(listener: PayloadListener) {
+    this._payloadProcessor.addPayloadListener(listener);
+  }
+
+  removePayloadListener(listener: PayloadListener) {
+    this._payloadProcessor.removePayloadListener(listener);
+  }
+
+  private _attachHandler(stream: EventStream, eventName: string) {
+    stream.addEventListener(eventName, async (event?: { data?: string }) => {
+      if (event?.data) {
+        this._logger?.debug(`Received ${eventName} event.  Data is ${event.data}`);
+        try {
+          this._payloadProcessor.processEvents([
+            { event: eventName, data: JSON.parse(event.data) },
+          ]);
+        } catch {
+          this._logger?.error(
+            `Stream received data that was unable to be processed in "${eventName}" message`,
+          );
+          this._logger?.debug(`Data follows: ${event.data}`);
+          this._errorHandler?.(DataSourceErrorKind.InvalidData, 'Malformed data in EventStream.');
+        }
+      } else {
+        this._errorHandler?.(DataSourceErrorKind.Unknown, 'Event from EventStream missing data.');
+      }
+    });
+  }
+}
