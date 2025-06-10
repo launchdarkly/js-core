78e9a5e93cb4c06a19c0d7d63307dfd3407d4505
Ryan Lamb <4955475+kinyoklion@users.noreply.github.com>
2025-06-10 13:38:25 -0700

    feat: Add support for an identify queue. (#842)
    Co-authored-by: Todd Anderson <127344469+tanderson-ld@users.noreply.github.com>

--- packages/sdk/browser/src/BrowserIdentifyOptions.ts
+++ packages/sdk/browser/src/BrowserIdentifyOptions.ts
@@ -1,5 +1,12 @@
 import { LDIdentifyOptions } from '@launchdarkly/js-client-sdk-common';
 
+/**
+ * @property sheddable - If true, the identify operation will be sheddable. This means that if multiple identify operations are done, without
+ * waiting for the previous one to complete, then intermediate results will be discarded. When false, identify
+ * operations will be queued and completed sequentially.
+ *
+ * Defaults to true.
+ */
 export interface BrowserIdentifyOptions extends Omit<LDIdentifyOptions, 'waitForNetworkResults'> {
   /**
    * The signed context key if you are using [Secure Mode]
