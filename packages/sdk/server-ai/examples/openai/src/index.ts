c682595d685b88128cd2460bb6deb36c6077976a
jsonbailey <jbailey@launchdarkly.com>
2025-11-05 03:21:19 +0000

    ensure we have a tracker before using it

--- packages/sdk/server-ai/examples/openai/src/index.ts
+++ packages/sdk/server-ai/examples/openai/src/index.ts
@@ -58,8 +58,12 @@ async function main(): Promise<void> {
     { myVariable: 'My User Defined Variable' },
   );
 
-  const { tracker } = aiConfig;
-  const completion = await tracker.trackOpenAIMetrics(async () =>
+  if (!aiConfig.enabled || !aiConfig.tracker) {
+    console.log('*** AI configuration is not enabled');
+    process.exit(0);
+  }
+
+  const completion = await aiConfig.tracker.trackOpenAIMetrics(async () =>
     client.chat.completions.create({
       messages: aiConfig.messages || [],
       model: aiConfig.model?.name || 'gpt-4',
