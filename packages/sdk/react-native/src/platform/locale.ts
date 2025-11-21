f8eca985271977baa1bca92a45d058236a207206
Steven Zhang <szhang@launchdarkly.com>
2025-11-21 12:05:45 -0600

    chore: addressing PR comments

--- packages/sdk/react-native/src/platform/locale.ts
+++ packages/sdk/react-native/src/platform/locale.ts
@@ -1,15 +1,18 @@
 import { NativeModules, Platform } from 'react-native';
+
 /**
- * Apps opted into Fabric (the new architecture of React Native) 
+ * Apps opted into Fabric (the new architecture of React Native)
  * may not have access to the SettingsManager.settings.AppleLocale property.
  * It is now common to use the `getConstants` method to access these constant properties with Fabric enabled apps.
  */
-const getAppleLocale = () => {
-  const settings = NativeModules.SettingsManager?.settings ?? NativeModules.SettingsManager?.getConstants()?.settings;
-  return settings?.AppleLocale;
-}
+const localeIdentifier = Platform.select({
+  ios: () => {
+    const settings =
+      NativeModules.SettingsManager?.settings ??
+      NativeModules.SettingsManager?.getConstants()?.settings;
+    return settings?.AppleLocale;
+  },
+  default: () => NativeModules.I18nManager?.localeIdentifier,
+});
 
-export default Platform.select({
-    ios: getAppleLocale(),
-    android: NativeModules.I18nManager?.localeIdentifier,
-})
+export default localeIdentifier();
