02ceefe712cd58278f2104e9c4195ca47e068898
Steven Zhang <szhang@launchdarkly.com>
2026-01-08 10:04:11 -0600

    chore: addressing PR comments

--- packages/sdk/svelte/src/lib/client/SvelteLDClient.ts
+++ packages/sdk/svelte/src/lib/client/SvelteLDClient.ts
@@ -1,8 +1,8 @@
 import { derived, type Readable, readonly, writable, type Writable } from 'svelte/store';
 
 import {
-  createClient as createClientSdk,
-  type LDClient,
+  createClient,
+  type LDClient as LDClientBase,
   type LDContext,
   type LDFlagSet,
   type LDFlagValue,
@@ -19,11 +19,57 @@ export type LDClientID = string;
 export type LDFlags = LDFlagSet;
 
 /**
+ * The LaunchDarkly client interface for Svelte which is a restrictive proxy of the {@link LDClientBase}.
+ */
+export interface LDClient {
+  /**
+   * Initializes the LaunchDarkly client.
+   * @param {LDClientID} clientId - The LD client-side ID.
+   * @param {LDContext} context - The user context.
+   * @param {LDOptions} options - The options.
+   * @returns {Promise} A promise that resolves when the client is initialized.
+   */
+  initialize(clientId: LDClientID, context: LDContext, options?: LDOptions): Promise<void>;
+
+  /**
+   * Identifies the user context.
+   * @param {LDContext} context - The user context.
+   * @returns {Promise} A promise that resolves when the user is identified.
+   */
+  identify(context: LDContext): Promise<LDIdentifyResult>;
+
+  /**
+   * The flags store.
+   */
+  flags: Readable<LDFlags>;
+
+  /**
+   * The initialization state store.
+   */
+  initalizationState: Readable<string>;
+
+  /**
+   * Watches a flag for changes.
+   * @param {string} flagKey - The key of the flag to watch.
+   * @returns {Readable<LDFlagValue>} A readable store of the flag value.
+   */
+  watch(flagKey: string): Readable<LDFlagValue>;
+
+  /**
+   * Gets the current value of a flag.
+   * @param {string} flagKey - The key of the flag to get.
+   * @param {TFlag} defaultValue - The default value of the flag.
+   * @returns {TFlag} The current value of the flag.
+   */
+  useFlag<TFlag extends LDFlagValue>(flagKey: string, defaultValue: TFlag): TFlag;
+}
+
+/**
  * Checks if the LaunchDarkly client is initialized.
  * @param {LDClient | undefined} client - The LaunchDarkly client.
  * @throws {Error} If the client is not initialized.
  */
-function isClientInitialized(client: LDClient | undefined): asserts client is LDClient {
+function isClientInitialized(client: LDClientBase | undefined): asserts client is LDClientBase {
   if (!client) {
     throw new Error('LaunchDarkly client not initialized');
   }
@@ -39,7 +85,7 @@ function isClientInitialized(client: LDClient | undefined): asserts client is LD
  * @param flags - The initial flags object to be proxied.
  * @returns A proxy object that intercepts access to flag values and returns the appropriate variation.
  */
-function toFlagsProxy(client: LDClient, flags: LDFlags): LDFlags {
+function toFlagsProxy(client: LDClientBase, flags: LDFlags): LDFlags {
   return new Proxy(flags, {
     get(target, prop, receiver) {
       const currentValue = Reflect.get(target, prop, receiver);
@@ -64,8 +110,8 @@ function toFlagsProxy(client: LDClient, flags: LDFlags): LDFlags {
  * Creates a LaunchDarkly instance.
  * @returns {Object} The LaunchDarkly instance object.
  */
-function init() {
-  let coreLdClient: LDClient | undefined;
+function init(): LDClient {
+  let coreLdClient: LDClientBase | undefined;
   const flagsWritable = writable<LDFlags>({});
   const initializeResult = writable<string>('pending');
 
@@ -82,7 +128,7 @@ function init() {
     context: LDContext,
     options?: LDOptions,
   ): Promise<void> {
-    coreLdClient = createClientSdk(clientId, context, options);
+    coreLdClient = createClient(clientId, context, options);
 
     coreLdClient.on('change', () => {
       const rawFlags = coreLdClient!.allFlags();
