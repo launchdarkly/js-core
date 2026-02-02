829383eb0639fc66b1c83d9b0c363c707379b4c0
Steven Zhang <szhang@launchdarkly.com>
2026-02-02 10:00:37 -0600

    ci: workaround contract test breakage

--- contract-tests/src/sdkClientEntity.ts
+++ contract-tests/src/sdkClientEntity.ts
@@ -328,22 +328,9 @@ export async function newSdkClientEntity(options: any): Promise<SdkClientEntity>
     options.configuration.startWaitTimeMs !== undefined
       ? options.configuration.startWaitTimeMs
       : 5000;
-
-
-  // TODO: synchronizer should be a list, but for this is a temporary workaround
-  // so that we can pass the contract tests in a more reasonable manner.
-  // SDK-1798
-  const adaptedConfigs = {
-    ...options.configuration,
-  };
-
-  adaptedConfigs.dataSystem.synchronizers = {
-    primary: options.configuration.dataSystem.synchronizers?.[0],
-    secondary: options.configuration.dataSystem.synchronizers?.[1],
-  }
   const client: LDClient = ld.init(
     options.configuration.credential || 'unknown-sdk-key',
-    makeSdkConfig(adaptedConfigs, options.tag),
+    makeSdkConfig(options.configuration, options.tag),
   );
   try {
     await client.waitForInitialization({ timeout });
