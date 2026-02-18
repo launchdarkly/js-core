6674353252be58330318c132afd582b41d4c9bbd
Steven Zhang <szhang@launchdarkly.com>
2026-02-18 10:31:15 -0600

    chore: addressing PR comments

--- packages/sdk/react/src/client/LDClient.ts
+++ packages/sdk/react/src/client/LDClient.ts
@@ -2,19 +2,18 @@ import { LDClient, LDContext, LDWaitForInitializationResult } from '@launchdarkl
 
 /**
  * The LaunchDarkly client interface for React.
- * 
+ *
  * @privateRemarks
  * We will provide 2 ways to create instances of LDClient:
  * 1. A `createClient` function that is similar to the js-client-sdk's `createClient` function.
  * 2. A `createLDProvider` function that creates a React Context
- * 
+ *
  */
 export interface LDReactClient extends LDClient {
-
   /**
    * Returns the initialization state of the client. This function is helpful to determine
    * whether LDClient can be used to evaluate flags on intial component render.
-   * 
+   *
    * @see {@link LDWaitForInitializationResult} for the possible values and their meaning
    *
    * @returns {Promise<LDWaitForInitializationResult>} The initialization state of the client.
--- packages/sdk/react/src/server/LDOptions.ts
+++ packages/sdk/react/src/server/LDOptions.ts
@@ -1,10 +1,11 @@
 import { LDOptions as LDOptionsCommon } from '@launchdarkly/node-server-sdk';
+
 import { LDContextProvider } from './LDContextProvider';
 
 export interface LDReactServerOptions extends LDOptionsCommon {
   /**
    * A provider for the Launchdarkly context.
-   * 
+   *
    * @remarks
    * This is left up to the developer to implement and will be used
    * to do server side flag evalations. LDClient will not initialize
