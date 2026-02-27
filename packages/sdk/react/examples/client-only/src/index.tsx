578bdbf344090207506383204f3b12ca38fdf9c0
Steven Zhang <szhang@launchdarkly.com>
2026-02-27 15:37:21 -0600

    chore: bot comment

--- packages/sdk/react/examples/client-only/src/index.tsx
+++ packages/sdk/react/examples/client-only/src/index.tsx
@@ -5,11 +5,9 @@ import App from './App';
 import './index.css';
 import { LDReactProvider } from './LDClient';
 
-(async () => {
-  const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-  root.render(
-    <LDReactProvider>
-      <App />
-    </LDReactProvider>,
-  );
-})();
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(
+  <LDReactProvider>
+    <App />
+  </LDReactProvider>,
+);
