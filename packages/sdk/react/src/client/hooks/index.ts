fc0f372d0323583bc1973068d383b3ea1b13bfc1
Steven Zhang <szhang@launchdarkly.com>
2026-03-04 15:06:00 -0600

    chore: removing deprecated hooks

--- packages/sdk/react/src/client/hooks/index.ts
+++ packages/sdk/react/src/client/hooks/index.ts
@@ -1,7 +1,3 @@
-export { useFlag } from './useFlag';
-export type { LDFlagType } from './useFlag';
-export { useFlagDetail } from './useFlagDetail';
-export { useFlags } from './useFlags';
 export { useInitializationStatus } from './useInitializationStatus';
 export type { InitializationStatus } from './useInitializationStatus';
 export { useLDClient } from './useLDClient';
