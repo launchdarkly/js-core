65f5a53d19ce73bbe9a44f031246f1707a360145
Steven Zhang <szhang@launchdarkly.com>
2026-03-26 16:51:28 -0500

    chore: bot comments

--- packages/sdk/react/src/client/hooks/useVariationCore.ts
+++ packages/sdk/react/src/client/hooks/useVariationCore.ts
@@ -17,8 +17,11 @@ export default function useVariationCore<T, R = T>(
   // `maybeSetInitializationResult` in `packages/shared/sdk-client/src/LDClientImpl.ts`.
   const ready = initializedState !== 'initializing';
 
-  // Using refs here to capture the latest defaultValue and evaluate function
-  // without making them dependencies of the effect.
+  // Refs are used here so their values are captured and stay updated in the
+  // effect hook. These are purposely not dependencies of the effect hook as
+  // they should not trigger any changes to the hook state.
+  const readyRef = useRef(ready);
+  readyRef.current = ready;
   const defaultValueRef = useRef(defaultValue);
   defaultValueRef.current = defaultValue;
   const evaluateRef = useRef(evaluate);
@@ -37,7 +40,7 @@ export default function useVariationCore<T, R = T>(
   });
 
   useEffect(() => {
-    if (didMountRef.current && ready) {
+    if (didMountRef.current && readyRef.current) {
       setValue(evaluateRef.current(client, key, defaultValueRef.current));
     }
     didMountRef.current = true;
