cd60b80c880282dbe55e24e5136c89f15de96993
Steven Zhang <szhang@launchdarkly.com>
2026-04-22 12:18:36 -0500

    refactor(test): enabling no-non-null-asserted-option-chain
    sdk-2232

--- .eslintrc.js
+++ .eslintrc.js
@@ -39,7 +39,6 @@ module.exports = {
     '@typescript-eslint/ban-ts-comment': 'off',
     '@typescript-eslint/ban-types': 'off',
     '@typescript-eslint/no-var-requires': 'off',
-    '@typescript-eslint/no-non-null-asserted-optional-chain': 'off',
     '@typescript-eslint/no-unused-vars': [
       'error',
       { ignoreRestSiblings: true, argsIgnorePattern: '^_', varsIgnorePattern: '^__' },
