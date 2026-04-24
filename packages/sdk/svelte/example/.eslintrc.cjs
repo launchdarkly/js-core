619c7e0b39a6dd90ad6d053dd20b1ff042300d6a
Steven Zhang <szhang@launchdarkly.com>
2026-04-24 17:24:42 -0500

    refactor: remove prettier dependency

--- packages/sdk/svelte/example/.eslintrc.cjs
+++ packages/sdk/svelte/example/.eslintrc.cjs
@@ -4,8 +4,7 @@ module.exports = {
 	extends: [
 		'eslint:recommended',
 		'plugin:@typescript-eslint/recommended',
-		'plugin:svelte/recommended',
-		'prettier'
+		'plugin:svelte/recommended'
 	],
 	parser: '@typescript-eslint/parser',
 	plugins: ['@typescript-eslint'],
