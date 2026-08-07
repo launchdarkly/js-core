0559003fe4f0e48cbdf2fddb422d2af1884930ad
Steven Zhang <szhang@launchdarkly.com>
2026-08-07 09:15:08 -0400

    chore: revert to use explicit exports

--- packages/sdk/react/src/client/index.ts
+++ packages/sdk/react/src/client/index.ts
@@ -1,4 +1,5 @@
-export * from '@launchdarkly/js-client-sdk';
+export type * from '@launchdarkly/js-client-sdk';
+export { basicLogger } from '@launchdarkly/js-client-sdk';
 export * from './LDClient';
 export * from './LDOptions';
 
